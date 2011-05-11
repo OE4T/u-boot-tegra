chpec/uboot-ac100

Factor out verified boot required hardware interface
BUG=none
TEST=emerge-tegra2_seaboard chromeos-u-boot-next

Review URL: http://codereview.chromium.org/6543034

Change-Id: I88dfd38aedd81281415ecb8156181a6cc4d46495

Makefile
@@ -186,6 +186,7 @@ LIBS += lib/lzma/liblzma.a
 LIBS += lib/lzo/liblzo.a
 ifdef VBOOT
 LIBS += lib/chromeos/libchromeos.a
+LIBS += board/$(BOARDDIR)/chromeos/libchromeos_hardware_interface.a
 endif
 LIBS += $(shell if [ -f board/$(VENDOR)/common/Makefile ]; then echo \
 	"board/$(VENDOR)/common/lib$(VENDOR).a"; fi)

OE4T/u-boot-tegra

Makefile
@@ -184,6 +184,7 @@ LIBS  = lib/libgeneric.o
 LIBS += lib/lzma/liblzma.o
 LIBS += lib/lzo/liblzo.o
 LIBS += lib/chromeos/libchromeos.a
+LIBS += board/$(VENDOR)/chromeos/libchromeos_hardware_interface.a
 LIBS += $(shell if [ -f board/$(VENDOR)/common/Makefile ]; then echo \
 	"board/$(VENDOR)/common/lib$(VENDOR).o"; fi)
 LIBS += $(CPUDIR)/lib$(CPU).o