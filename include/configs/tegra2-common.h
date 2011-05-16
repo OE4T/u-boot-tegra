c07dd4f6aed384e6dd3d3905f6cf27b0ceb4b2de
Simon Glass <sjg@chromium.org>
2011-05-16 14:45:57 -0700

    config: tegra2: Enable silent console
    This enables silent console - if FDT is used it will be controlled from
    the FDT blob.
    
    BUG=chromium-os:11623
    TEST=build and boot U-Boot on Seaboard
    
    Change-Id: I09b9abf51faa99788ac1b235e74620772a96fc17
    Reviewed-on: http://gerrit.chromium.org/gerrit/964
    Tested-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- include/configs/tegra2-common.h
+++ include/configs/tegra2-common.h
@@ -223,6 +223,7 @@
 #define CONFIG_SYS_HUSH_PARSER		/* use "hush" command parser */
 #define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
 #define CONFIG_SYS_PROMPT		V_PROMPT
+#define CONFIG_SILENT_CONSOLE
 /*
  * Increasing the size of the IO buffer as default nfsargs size is more
  *  than 256 and so it is not possible to edit it
