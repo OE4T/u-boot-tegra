3d52427c2b919a416a5c5f9a2e6b2d99185234a6
Anton Staaf <robotboy@chromium.org>
2011-06-03 11:38:46 -0700

    mmc: Tegra2: Factor out mmc_wait_inhibit functionality
    This is a well encapsulated section of mmc_send_cmd, by moving
    it to it's own function it increases the readability of mmc_send_cmd.
    
    BUG=None
    TEST=Run "mmc part 0" on Seaboard
    
    Signed-off-by: Anton Staaf <robotboy@chromium.org>
    Change-Id: I76ff1f8bdebba9814c45abab5f198c58c840567a
    Reviewed-on: http://gerrit.chromium.org/gerrit/2113

--- drivers/mmc/tegra2_mmc.c
+++ drivers/mmc/tegra2_mmc.c
@@ -103,18 +103,12 @@ static void mmc_set_transfer_mode(struct mmc_host *host, struct mmc_data *data)
 	writew(mode, &host->reg->trnmod);
 }
 
-static int mmc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd,
-			struct mmc_data *data)
+static int mmc_wait_inhibit(struct mmc_host *host,
+			    struct mmc_cmd *cmd,
+			    struct mmc_data *data,
+			    unsigned int timeout)
 {
-	struct mmc_host *host = (struct mmc_host *)mmc->priv;
-	int flags, i;
-	unsigned int timeout;
-	unsigned int mask;
-	unsigned int retry = 0x100000;
-	debug(" mmc_send_cmd called\n");
-
-	/* Wait max 10 ms */
-	timeout = 10;
+	unsigned int mask = 0;
 
 	/*
 	 * PRNSTS
@@ -122,15 +116,13 @@ static int mmc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd,
 	 * CMDINHCMD[0]	: Command Inhibit (CMD)
 	 */
 	mask = (1 << 0);
-	if ((data != NULL) || (cmd->resp_type & MMC_RSP_BUSY))
-		mask |= (1 << 1);
 
 	/*
 	 * We shouldn't wait for data inhibit for stop commands, even
 	 * though they might use busy signaling
 	 */
-	if (data)
-		mask &= ~(1 << 1);
+	if ((data == NULL) && (cmd->resp_type & MMC_RSP_BUSY))
+		mask |= (1 << 1);
 
 	while (readl(&host->reg->prnsts) & mask) {
 		if (timeout == 0) {
@@ -140,6 +132,23 @@ static int mmc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd,
 		timeout--;
 		udelay(1000);
 	}
+
+	return 0;
+}
+
+static int mmc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd,
+			struct mmc_data *data)
+{
+	struct mmc_host *host = (struct mmc_host *)mmc->priv;
+	int flags, i;
+	int result;
+	unsigned int timeout;
+	unsigned int mask;
+	unsigned int retry = 0x100000;
+	debug(" mmc_send_cmd called\n");
+
+	if ((result = mmc_wait_inhibit(host, cmd, data, 10 /* ms */)) < 0)
+		return result;
 
 	if (data)
 		mmc_prepare_data(host, data);
