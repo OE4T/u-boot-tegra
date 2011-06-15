9a9a94071e84021485014b268f48a6c652f9cf6b
Tom Wai-Hong Tam <waihong@chromium.org>
2011-06-16 01:03:53 +0800

    CHROMIUM: Add chromeos_seaboard_vboot config for vboot_wrapper refactoring
    This config is derived from seaboard. We use this config to separate the
    changes made for vboot_wrapper refactoring.
    
    BUG=chromium-os:16542
    TEST=build it successfully:
    $ make ARCH=arm \
           CROSS_COMPILE=armv7a-cros-linux-gnueabi- \
           USE_PRIVATE_LIBGCC=yes \
           HOSTSTRIP=true \
           distclean
    $ make ARCH=arm \
           CROSS_COMPILE=armv7a-cros-linux-gnueabi- \
           USE_PRIVATE_LIBGCC=yes \
           HOSTSTRIP=true \
           chromeos_seaboard_vboot_config
    $ make ARCH=arm \
           CROSS_COMPILE=armv7a-cros-linux-gnueabi- \
           USE_PRIVATE_LIBGCC=yes \
           HOSTSTRIP=true \
           all
    
    Change-Id: Ifbb310b4f95ef3500b444f37cd465bb903bbaa84
    Reviewed-on: http://gerrit.chromium.org/gerrit/2684
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

---
+++ include/configs/chromeos_seaboard_vboot.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __configs_chromeos_seaboard_vboot_h__
+#define __configs_chromeos_seaboard_vboot_h__
+
+#include <configs/seaboard.h>
+
+#define CONFIG_VBOOT_WRAPPER
+
+#endif /* __configs_chromeos_seaboard_vboot_h__ */
