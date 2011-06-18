08792bb65083723610cce1dee9624b4f76da9efc
Gabe Black <gabeblack@chromium.org>
2011-06-17 22:21:34 -0700

    Turn on support for EFI's GPT (GUID partition table).
    This allows u-boot to figure out the partitions of a chrome-os install.
    
    BUG=chrome-os-partner:3895
    TEST=Ran the "ide part" command and saw it print reasonable looking
    information about the GPT.
    
    Change-Id: Ie08645bec58b746ac819118c8cf62ec336f6a60e
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: http://gerrit.chromium.org/gerrit/2856
    Reviewed-by: Stefan Reinauer <reinauer@google.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>

--- include/configs/coreboot.h
+++ include/configs/coreboot.h
@@ -86,6 +86,7 @@
 /************************************************************
  * DISK Partition support
  ************************************************************/
+#define CONFIG_EFI_PARTITION
 #define CONFIG_DOS_PARTITION
 #define CONFIG_MAC_PARTITION
 #define CONFIG_ISO_PARTITION		/* Experimental */
