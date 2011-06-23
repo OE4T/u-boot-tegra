ff78cecaac81e7daf3b6060a91c1f2002058bca6
Che-Liang Chiou <clchiou@chromium.org>
2011-06-22 17:45:31 -0700

    CHROMIUM: update and tidy up gbb_bmpblk
    Changes include:
    * CONFIG_CMD_BMP is not defined and so every reference to it is removed
    * remove #ifdef guards; if any symbol is missing, build should fail
    
    Note: debug output messes up bitmaps display, but this is intended. Most
    test users do not have the privilege to access serial output, and so LCD
    is their only source of debug message. We will turn off debug output on
    LCD after we feel that firmware is stable enough that debug messages are
    not useful to test users.
    
    BUG=chromium-os:16508
    TEST=bitmaps are displayed in developer and recovery mode on seaboard
    
    Change-Id: I80be6544298bd6cf3db24f650583dbad247f431c
    Reviewed-on: http://gerrit.chromium.org/gerrit/3067
    Tested-by: Che-Liang Chiou <clchiou@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- lib/chromeos/gbb_bmpblk.c
+++ lib/chromeos/gbb_bmpblk.c
@@ -15,21 +15,17 @@
 #include <malloc.h>
 #include <chromeos/common.h>
 #include <chromeos/gbb_bmpblk.h>
-
-#include <bmpblk_header.h>
-#include <gbb_header.h>
-
-#ifdef CONFIG_LZMA
 #include <lzma/LzmaTypes.h>
 #include <lzma/LzmaDec.h>
 #include <lzma/LzmaTools.h>
-#endif /* CONFIG_LZMA */
 
-#ifdef CONFIG_CMD_BMP
+/* headers of vboot_reference */
+#include <bmpblk_header.h>
+#include <gbb_header.h>
+
+/* defined in common/lcd.c */
 extern int lcd_display_bitmap (ulong, int, int);
-#endif /* CONFIG_CMD_BMP */
 
-#ifdef CONFIG_LZMA
 static uint8_t *uncompress_lzma(uint8_t *in_addr, SizeT in_size,
 				SizeT out_size)
 {
@@ -44,7 +40,6 @@ static uint8_t *uncompress_lzma(uint8_t *in_addr, SizeT in_size,
         }
         return out_addr;
 }
-#endif /* CONFIG_LZMA */
 
 int print_screen_info_in_bmpblk(uint8_t *gbb_start, int index)
 {
@@ -85,7 +80,6 @@ int display_screen_in_bmpblk(uint8_t *gbb_start, int index)
 	ScreenLayout *screen;
 	ImageInfo *info;
 	uint8_t *data;
-	int needed_free;
 	int i;
 	int ret;
 
@@ -102,36 +96,29 @@ int display_screen_in_bmpblk(uint8_t *gbb_start, int index)
 	     ++i) {
 		info = (ImageInfo *)((uint8_t *)bmph +
 				     screen->images[i].image_info_offset);
-		data = NULL;
-		needed_free = 0;
-		if (info->compression == COMPRESS_NONE) {
-			data = (uint8_t *)(info + 1);
-		}
-#ifdef CONFIG_LZMA
+
+		if (info->compression != COMPRESS_NONE &&
+				info->compression != COMPRESS_LZMA1)
+			return BMPBLK_UNSUPPORTED_COMPRESSION;
+
+		data = (uint8_t *)(info + 1);
 		if (info->compression == COMPRESS_LZMA1) {
-			data = uncompress_lzma(
-				(uint8_t *)(info + 1),
+			data = uncompress_lzma(data,
 				(SizeT)info->compressed_size,
 				(SizeT)info->original_size);
-			if (data) needed_free = 1;
-			else return BMPBLK_LZMA_DECOMPRESS_FAILED;
 		}
-#endif /* CONFIG_LZMA */
-		if (data) {
-			ret = -1;
-#ifdef CONFIG_CMD_BMP
-			ret = lcd_display_bitmap((ulong)data,
+		if (!data)
+			return BMPBLK_LZMA_DECOMPRESS_FAILED;
+
+		ret = lcd_display_bitmap((ulong)data,
 				screen->images[i].x,
 				screen->images[i].y);
-#endif /* CONFIG_CMD_BMP */
-			if (ret) {
-				if (needed_free) free(data);
-				return BMPBLK_BMP_DISPLAY_FAILED;
-			}
-		} else {
-			return BMPBLK_UNSUPPORTED_COMPRESSION;
-		}
-		if (needed_free) free(data);
+
+		if (info->compression == COMPRESS_LZMA1)
+			free(data);
+
+		if (ret)
+			return BMPBLK_BMP_DISPLAY_FAILED;
 	}
 	return BMPBLK_OK;
 }
