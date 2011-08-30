e9454f87ecf031c3105b02ecdbcf74810ff69959
Tom Warren <twarren@nvidia.com>
2011-08-30 15:53:25 -0700

    armv7: cpu: Tegra3: remove DEBUG #define
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Change-Id: Ideefb8dad249358ca395de46b4211349188b6fea
    Reviewed-on: http://git-master/r/49932
    Reviewed-by: Yen Lin <yelin@nvidia.com>

--- arch/arm/cpu/armv7/tegra3/board.c
+++ arch/arm/cpu/armv7/tegra3/board.c
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
-#define DEBUG			//tcw
+
 #include <common.h>
 #include <asm/io.h>
 #include <asm/arch/sys_proto.h>
