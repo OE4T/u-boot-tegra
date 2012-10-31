8692ea4e529b5dfda9835960427be78c3a38e6e6
Tom Warren <twarren@nvidia.com>
2012-10-31 15:24:09 -0700

    Tegra: T114: Add Dalmore build (E1611 T114)
    Based on the Dalmore E1611 bringup done on 12Oct @ HQ,
    this is basically the Curacao build renamed to Dalmore.
    
    It's not fully functional - Jimmy Zhang will add his
    changes later that brought the E1611 Dalmore board to
    the command prompt.
    
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Change-Id: If6a7421c3b3700ffc9b26d0f217a82bfe508a5c2
    Reviewed-on: http://git-master/r/160371
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jimmy Zhang <jimmzhang@nvidia.com>
    Reviewed-by: Jong Kim <jongk@nvidia.com>
    Reviewed-on: http://git-master/r/166663
    Tested-by: Jong Kim <jongk@nvidia.com>

---
+++ include/configs/dalmore.h
@@ -0,0 +1,156 @@
+/*
+ *  (C) Copyright 2010,2011
+ *  NVIDIA Corporation <www.nvidia.com>
+ * Portions Copyright (c) 2011 The Chromium OS Authors
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <asm/sizes.h>
+
+/* High-level configuration options */
+#define V_PROMPT	"Dalmore #"
+
+#define CONFIG_TEGRA2_LP0
+#define CONFIG_SYS_SKIP_ARM_RELOCATION
+
+#define CONFIG_TEGRA11X_DALMORE
+#if t11x_port
+/*tcw - fix this, s/b 408MHz on Dalmore?*/
+#else
+#define CONFIG_SYS_PLLP_BASE_IS_408MHZ
+#endif
+#define z_pause()	\
+{			\
+	while (1)	\
+		;	\
+}
+
+#define CONFIG_EXTRA_BOOTARGS \
+	"panel=lvds " \
+	"tegraid=30.1.2.0.0 " \
+	"debug_uartport=lsport\0" \
+
+#include "tegra11x-common.h"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	CONFIG_EXTRA_ENV_SETTINGS_COMMON \
+	"board=dalmore\0" \
+
+#define CONFIG_DEFAULT_DEVICE_TREE "t114-dalmore"
+
+#define CONFIG_SERIAL_TAG	/* enable passing serial# (board id) */
+#define CONFIG_BOARD_ID_LOW_VERBIER_A02		0x02420700
+#if t11x_port
+#define TEGRA3_BOOT_TRACE
+#define TEGRA3_BOOT_TRACE_MORE
+
+#define CONFIG_TEGRA_SERIAL_HIGH	0xffffffff
+#define CONFIG_TEGRA_SERIAL_LOW		0xffffffff
+#define EEPROM_I2C_BUS		0
+#else
+#define CONFIG_TEGRA_SERIAL_HIGH	CONFIG_BOARD_ID_HIGH_A04
+#define CONFIG_TEGRA_SERIAL_LOW		CONFIG_BOARD_ID_LOW_A04
+#endif
+/* The following are used to retrieve the board id from an eeprom */
+#define CONFIG_SERIAL_EEPROM
+#define EEPROM_I2C_BUS		0
+#define EEPROM_I2C_ADDRESS	0x56
+#define EEPROM_SERIAL_OFFSET	0x04
+#define NUM_SERIAL_ID_BYTES	8
+
+#ifndef CONFIG_OF_CONTROL
+/* Things in here are defined by the device tree now. Let it grow! */
+
+#define CONFIG_TEGRA2_ENABLE_UARTD
+#define CONFIG_SYS_NS16550_COM1		NV_PA_APB_UARTD_BASE
+
+/* To select the order in which U-Boot sees USB ports */
+#define CONFIG_TEGRA2_USB0      NV_PA_USB3_BASE
+#define CONFIG_TEGRA2_USB1      NV_PA_USB1_BASE
+#define CONFIG_TEGRA2_USB2      0
+#define CONFIG_TEGRA2_USB3      0
+
+/* Put USB1 in host mode */
+#define CONFIG_TEGRA2_USB1_HOST
+#define CONFIG_MACH_TYPE	MACH_TYPE_CARDHU
+
+#endif /* CONFIG_OF_CONTROL not defined ^^^^^^^ */
+#define CONFIG_CONSOLE_MUX
+#define CONFIG_SYS_CONSOLE_IS_IN_ENV
+#define CONFIG_STD_DEVICES_SETTINGS	"stdin=serial,tegra-kbc\0" \
+					"stdout=serial,lcd\0" \
+					"stderr=serial,lcd\0"
+
+#define CONFIG_ENV_SECT_SIZE    CONFIG_ENV_SIZE
+#define CONFIG_ENV_OFFSET       (SZ_4M - CONFIG_ENV_SECT_SIZE)
+
+#define TEGRA_MMC_DEFAULT_DEVICE	"1"	/* TCW SD-card for now */
+/* GPIO */
+#define CONFIG_TEGRA2_GPIO
+#define CONFIG_CMD_TEGRA2_GPIO_INFO
+
+/* SPI */
+#define CONFIG_TEGRA_SPI
+#define CONFIG_USE_SLINK	/* Cardhu SPI chip is on SBC4 */
+#define CONFIG_SPI_FLASH
+#define CONFIG_SPI_FLASH_ATMEL
+#define CONFIG_SPI_FLASH_WINBOND
+#define CONFIG_SF_DEFAULT_MODE		SPI_MODE_0
+#define CONFIG_CMD_SPI
+#define CONFIG_CMD_SF
+/* Environment in SPI */
+#define CONFIG_ENV_IS_IN_SPI_FLASH
+
+/* I2C */
+#define CONFIG_TEGRA2_I2C
+#define CONFIG_SYS_I2C_INIT_BOARD
+#define CONFIG_I2C_MULTI_BUS
+#define CONFIG_SYS_MAX_I2C_BUS		5
+#define CONFIG_SYS_I2C_SPEED		100000
+#define CONFIG_CMD_I2C
+
+/* SD/MMC */
+#define CONFIG_MMC
+#define CONFIG_GENERIC_MMC
+#define CONFIG_TEGRA2_MMC
+#define CONFIG_CMD_MMC
+
+#define CONFIG_DOS_PARTITION
+#define CONFIG_EFI_PARTITION
+#define CONFIG_CMD_EXT2
+#define CONFIG_CMD_FAT
+
+#if 0 /* tcw - disable kbd and LCD for now */
+#define CONFIG_TEGRA_KEYBOARD
+#define CONFIG_KEYBOARD
+/*
+ *  LCDC configuration
+ */
+#define CONFIG_LCD
+#define CONFIG_VIDEO_TEGRA2
+/* TODO: This needs to be configurable at run-time */
+#define LCD_BPP	LCD_COLOR16
+#define CONFIG_SYS_WHITE_ON_BLACK       /*Console colors*/
+#endif /* tcw if 0 kbd */
+
+#endif /* __CONFIG_H */
