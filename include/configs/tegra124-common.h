TobleMiner/u-boot-cmm2

tegra124: Expand SPL space by 8KB
We are getting very close to running out of space in SPL, and with the
currently Chrome OS gcc 4.9 we exceed the limit. Add a litle more space.

Signed-off-by: Simon Glass <sjg@chromium.org>
Signed-off-by: Tom Warren <twarren@nvidia.com>

include/configs/tegra124-common.h
@@ -26,7 +26,7 @@
 /*-----------------------------------------------------------------------
  * Physical Memory Map
  */
-#define CONFIG_SYS_TEXT_BASE	0x8010E000
+#define CONFIG_SYS_TEXT_BASE	0x80110000
 
 /*
  * Memory layout for where various images get loaded by boot scripts:

OE4T/u-boot-tegra

Bug 200051179
Change-Id: Ia826afc405188dd9317656d8a889d87a98cc2b0c
(cherry picked from commit ba521994229cbb707d04fb7e30395bba7007c776 upstream)
[swarren, applied the equivalent change to L4T's modified TEXT_BASE values]
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: http://git-master/r/780073
GVS: Gerrit_Virtual_Submit

include/configs/tegra124-common.h
@@ -26,7 +26,7 @@
 /*-----------------------------------------------------------------------
  * Physical Memory Map
  */
-#define CONFIG_SYS_TEXT_BASE	0x83D8E000
+#define CONFIG_SYS_TEXT_BASE	0x83D90000
 
 /*
  * Memory layout for where various images get loaded by boot scripts: