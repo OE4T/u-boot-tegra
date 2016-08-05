OE4T/u-boot-tegra

commit 39125874f8657d2f0c4479aac9617371b39751ee

ARM: tegra: add support for nvtboot on Tegra210
nvtboot is a bootloader for NVIDIA Tegra platforms. Its purpose is to
perform OS-independent system configuration. Among other things, nvtboot
may boot a secure OS and set up various memory carve-outs. Once nvtboot
has completed execution, it typically chain-loads a use-case-specific
bootloader. U-Boot is one such bootloader. When doing so, it passes on
various configuration information (such as RAM carve-out locations and
sizes) via an in-RAM data structure, pointed at by a CPU register.

Enhance U-Boot to support parsing the data structure passed on by nvtboot
for RAM layout information. Future patches will make use of other data
supplied by nvtboot.

RAM region sizes are aligned to 2M, since this matches what NVIDIA's
binary bootloader does. Besides, the kernel probably likes sections.

Bug 200051179
Bug 200211165

Change-Id: I30ae511926fd30d9ab68e14069d94c49a008ab37
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: http://git-master/r/789237
(cherry picked from commit fcf210b7f5b532a7a08da63efbf9704f9a53d782 in main-v2015.07-rc2)
(swarren, tweaked to isolate the code to T210, and enable it for all T210)
Reviewed-on: http://git-master/r/1195412
Reviewed-by: Tom Warren <twarren@nvidia.com>

arch/arm/mach-tegra/tegra210/nvtboot.c
@@ -1,0 +1,341 @@
+/*
+ * nvtboot-related code
+ *
+ * (C) Copyright 2015 NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <stdlib.h>
+#include <common.h>
+#include "nvtboot.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+typedef enum {
+	/* Invalid entry */
+	MEM_LAYOUT_NONE = 0x0,
+	/* Primary memory layout */
+	MEM_LAYOUT_PRIMARY,
+	/* Extended memory layout */
+	MEM_LAYOUT_EXTENDED,
+	/* VPR carveout information */
+	MEM_LAYOUT_VPR,
+	/* TSEC carveout information */
+	MEM_LAYOUT_TSEC,
+	/* SECUREOS carveout information */
+	MEM_LAYOUT_SECUREOS,
+	/* LP0 carveout information */
+	MEM_LAYOUT_LP0,
+	/* NCK carveout information */
+	MEM_LAYOUT_NCK,
+	/* DEBUG carveout information */
+	MEM_LAYOUT_DEBUG,
+	/* BPMP FW carveout information */
+	MEM_LAYOUT_BPMPFW,
+	/* RAMDump carveout information */
+	MEM_LAYOUT_NVDUMPER,
+	/* carveout for GSC1 */
+	MEM_LAYOUT_GSC1,
+	/* carveout for GSC2 */
+	MEM_LAYOUT_GSC2,
+	/* carveout for GSC3 */
+	MEM_LAYOUT_GSC3,
+	/* carveout for GSC4 */
+	MEM_LAYOUT_GSC4,
+	/* carveout for GSC5 */
+	MEM_LAYOUT_GSC5,
+	/* Total types of memory layout */
+	MEM_LAYOUT_NUM,
+} mem_layout;
+
+/**
+ * Reason for poweron
+ */
+typedef enum {
+	poweron_source_pmcpor,
+	poweron_source_apwdt_rst,
+	poweron_source_sensor_rst,
+	poweron_source_sw_rst,
+	poweron_source_deepsleep_rst,
+	poweron_source_aotag_rst,
+	poweron_source_onkey,
+	poweron_source_resetkey,
+	poweron_source_usbhotplug,
+	poweron_source_rtcalarm,
+	poweron_source_pmuwdt,
+	poweron_source_unknown = 0x7fffffff,
+} poweron_source;
+
+/**
+ * Tracks the base and size of the Carveout
+ */
+typedef struct {
+	/* Base of the carveout */
+	 uint64_t base;
+	/* Size of the carveout */
+	 uint64_t size;
+} carveout_info;
+
+/**
+ * Stores the information related to memory params
+ */
+typedef struct {
+	/* Size and Base of the carveout */
+	carveout_info car_info[MEM_LAYOUT_NUM];
+	/* DRAM bottom information */
+	uint64_t dram_carveout_bottom;
+} mem_info;
+
+/**
+ * Stores the information related to memory params
+ */
+typedef enum {
+	/* Specifies the memory type to be undefined */
+	dram_memory_type_none = 0,
+	/* Specifies the memory type to be LPDDR2 SDRAM */
+	dram_memory_type_lpddr2,
+	/* Specifies the memory type to be DDR3 SDRAM */
+	dram_memory_type_ddr3,
+	dram_memory_type_lpddr4,
+	dram_memory_type_num,
+	dram_memory_type_force32 = 0x7FFFFFF
+} dram_memory_type;
+
+/**
+ * Defines the BootDevice Type available
+ */
+typedef enum {
+	/* boot device is sdmmc */
+	BOOT_DEVICE_SDMMC,
+	/* boot device is snor */
+	BOOT_DEVICE_SNOR_FLASH,
+	/* boot device is spi */
+	BOOT_DEVICE_SPI_FLASH,
+	/* boot device is sata */
+	BOOT_DEVICE_SATA,
+	/* boot device is usb3 */
+	BOOT_DEVICE_USB3,
+	/*reserved boot device */
+	BOOT_DEVICE_RESVD,
+	/* max number of boot devices */
+	BOOT_DEVICE_MAX,
+} fuse_boot_device;
+
+/**
+ * Device type and instance
+ */
+typedef struct {
+	/* boot of storage device type */
+	fuse_boot_device device;
+	/* instance of the device */
+	uint32_t instance;
+} dev_info;
+
+/**
+ * Holds the chip's unique ID.
+ */
+typedef struct {
+	/* Bytes 3-0 of the ecid */
+	uint32_t ecid_0;
+	/* Bytes 7-4 of ecid */
+	uint32_t ecid_1;
+	/* Bytes 11-8 of ecid */
+	uint32_t ecid_2;
+	/* Bytes 15-12 of ecid */
+	uint32_t ecid_3;
+} unique_chip;
+
+/**
+ * Defines the board information stored on eeprom
+ */
+typedef struct {
+	/* Holds the version number. */
+	uint16_t version;
+	/* Holds the size of 2 byte value of \a board_id data that follows. */
+	uint16_t size;
+	/* Holds the board number. */
+	uint16_t board_id;
+	/* Holds the SKU number. */
+	uint16_t sku;
+	/* Holds the FAB number. */
+	uint8_t fab;
+	/* Holds the part revision. */
+	uint8_t revision;
+	/* Holds the minor revision level. */
+	uint8_t minor_revision;
+	/* Holds the memory type. */
+	uint8_t mem_type;
+	/* Holds the power configuration values. */
+	uint8_t power_config;
+	/* Holds the SPL reworks, mech. changes. */
+	uint8_t misc_config;
+	/* Holds the modem bands value. */
+	uint8_t modem_bands;
+	/* Holds the touch screen configs. */
+	uint8_t touch_screen_configs;
+	/* Holds the display configs. */
+	uint8_t display_configs;
+} board_info;
+
+/**
+ * Specifies different modes of booting.
+ */
+typedef enum {
+	/* Specifies a default (unset) value. */
+	BOOT_MODE_NONE = 0,
+	/* Specifies a cold boot */
+	BOOT_MODE_COLD,
+	/* Specifies the BR entered RCM */
+	BOOT_MODE_RECOVERY,
+	/* Specifies UART boot (only available internal to NVIDIA) */
+	BOOT_MODE_UART,
+} boot_mode;
+
+/**
+ * Specifies different operating modes.
+ */
+typedef enum {
+	/* Invalid Operating mode */
+	OPMODE_NONE = 0,
+	/* Preproduction mode */
+	OPMODE_PRE_PRODUCTION,
+	/* failure analysis mode */
+	OPMODE_FAILURE_ANALYSIS,
+	/* nvproduction mode */
+	OPMODE_NV_PRODUCTION,
+	/* non-secure mode */
+	OPMODE_ODM_PRODUCTION_NON_SECURE,
+	/* sbk mode */
+	OPMODE_ODM_PRODUCTION_SECURE_SBK,
+	/* pkc mode */
+	OPMODE_ODM_PRODUCTION_SECURE_PKC,
+} opmode_type;
+
+typedef struct {
+	/* Active BCT block */
+	uint32_t active_bct_block;
+	/* Active BCT sector */
+	uint32_t active_bct_sector;
+} active_bct_info;
+
+/**
+ * Specifies shared structure.
+ */
+typedef struct {
+	/* Revision of common structure */
+	uint32_t revision;
+	/* CRC of this structure */
+	uint32_t crc;
+	/* Early Uart Address */
+	uint64_t early_uart_addr;
+	/* Memory layout Information */
+	mem_info params;
+	/* Poweron Source */
+	poweron_source pwron_src;
+	/* Pmic shutdown reason */
+	uint8_t pmic_reset_reason;
+	/* Information about bootdevice */
+	dev_info boot_dev;
+	/* Information about storage device */
+	dev_info storage_dev;
+	/* Unique Chip Id */
+	unique_chip uid;
+	dram_memory_type dram_type;
+	/* Processor Board Details */
+	board_info proc_board_details;
+	/* Pmu Board Details */
+	board_info pmu_board_details;
+	/* Display BoardDetails */
+	board_info display_board_details;
+	/* Size of Bct in bytes */
+	uint32_t bct_size;
+	/* Size of start Offset of tboot in bytes */
+	uint64_t tboot_start_offset_inbytes;
+	/* Size of boot device block size in  bytes */
+	uint32_t block_size_inbytes;
+	/* Size of boot device page size in bytes */
+	uint32_t page_size_inbytes;
+	/* Boot mode can be cold boot, uart or recovery */
+	boot_mode boot_type;
+	/* Operating Mode of device */
+	opmode_type op_mode;
+	/* Size of start Offset of mts preboot in bytes */
+	uint64_t mts_start_offset_inbytes;
+	/* Info of the bct being used */
+	active_bct_info active_bct;
+	/* Bootloader dtb load address */
+	uint32_t bl_dtb_load_addr;
+	/* Kernel dtb load address */
+	uint32_t kernel_dtb_load_addr;
+	/* Reserved Bytes at the end */
+	uint8_t reserved[256];
+} boot_arg;
+
+extern boot_arg *nvtboot_boot_x5;
+/*
+ * This must be in .data not .bss so we can access it before relocation, both
+ * due to MMU permissions, and because we don't want to trash any data that's
+ * appended to the U-Boot binary (DTB, binary data for flashing, ...) Hence,
+ * assign some meaningless value to it.
+ */
+static boot_arg nvtboot_boot_arg = {0xff};
+
+void nvtboot_init(void)
+{
+	memcpy(&nvtboot_boot_arg, nvtboot_boot_x5, sizeof(nvtboot_boot_arg));
+	/* FIXME: Should validate the structure here (version, CRC) */
+}
+
+/*
+ * Initialize U-Boot's DRAM bank configuration from the information supplied
+ * by nvtboot.
+ *
+ * nvtboot happens to segment RAM into two banks:
+ * MEM_LAYOUT_PRIMARY describes any usable RAM below 4GiB.
+ * MEM_LAYOUT_EXTENDED describes any RAM above 4GiB.
+ *
+ * This split is driven by the following requirements:
+ * - The NVIDIA L4T kernel requires separate entries in the DT /memory/reg
+ *   property for memory below and above the 4GiB boundary. The layout of that
+ *   DT property is directly driven by the entries in the U-Boot bank array.
+ * - The potential existence of a carve-out at the end of RAM below 4GiB can
+ *   only be represented using multiple banks.
+ */
+void dram_init_banksize(void)
+{
+	carveout_info *ci;
+
+	ci = &nvtboot_boot_arg.params.car_info[MEM_LAYOUT_PRIMARY];
+	gd->bd->bi_dram[0].start = ci->base;
+	gd->bd->bi_dram[0].size = ci->size & ~(SZ_2M - 1);
+
+#ifdef CONFIG_PHYS_64BIT
+	ci = &nvtboot_boot_arg.params.car_info[MEM_LAYOUT_EXTENDED];
+	gd->bd->bi_dram[1].start = ci->base;
+	gd->bd->bi_dram[1].size = ci->size & ~(SZ_2M - 1);
+#else
+	gd->bd->bi_dram[1].start = 0;
+	gd->bd->bi_dram[1].size = 0;
+#endif
+}
+
+/*
+ * Most hardware on 64-bit Tegra is still restricted to DMA to the lower
+ * 32-bits of the physical address space. Cap the maximum usable RAM area
+ * at 4 GiB to avoid DMA buffers from being allocated beyond the 32-bit
+ * boundary that most devices can address. Also, don't let U-Boot use any
+ * carve-out, as mentioned above. This conveniently matches the content of
+ * the MEM_LAYOUT_PRIMARY data supplied by nvtboot.
+ *
+ * This function is called before dram_init_banksize(), so we can't simply
+ * return gd->bd->bi_dram[1].start + gd->bd->bi_dram[1].size.
+ */
+ulong board_get_usable_ram_top(ulong total_size)
+{
+	carveout_info *ci;
+
+	ci = &nvtboot_boot_arg.params.car_info[MEM_LAYOUT_PRIMARY];
+
+	return (ci->base + ci->size) & ~(SZ_2M - 1);
+}

arch/arm/mach-tegra/board.c
@@ -36,7 +36,7 @@ enum {
 
 static bool from_spl __attribute__ ((section(".data")));
 
-#ifndef CONFIG_SPL_BUILD
+#if !defined(CONFIG_SPL_BUILD) && !defined(CONFIG_TEGRA210)
 void save_boot_params(u32 r0, u32 r1, u32 r2, u32 r3)
 {
 	from_spl = r0 != UBOOT_NOT_LOADED_FROM_SPL;

arch/arm/mach-tegra/board2.c
@@ -1,5 +1,5 @@
 /*
- *  (C) Copyright 2010,2011
+ *  (C) Copyright 2010,2011,2015
  *  NVIDIA Corporation <www.nvidia.com>
  *
  * SPDX-License-Identifier:	GPL-2.0+
@@ -41,6 +41,9 @@
 #include <i2c.h>
 #include <spi.h>
 #include "emc.h"
+#ifdef CONFIG_TEGRA210
+#include "tegra210/nvtboot.h"
+#endif
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -190,6 +193,10 @@ void gpio_early_init(void) __attribute__((weak, alias("__gpio_early_init")));
 
 int board_early_init_f(void)
 {
+#ifdef CONFIG_TEGRA210
+	nvtboot_init();
+#endif
+
 #if defined(CONFIG_TEGRA_DISCONNECT_UDC_ON_BOOT)
 #define USBCMD_FS2 (1 << 15)
 	{
@@ -278,6 +285,7 @@ void pad_init_mmc(struct mmc_host *host)
 }
 #endif	/* MMC */
 
+#ifndef CONFIG_TEGRA210
 /*
  * In some SW environments, a memory carve-out exists to house a secure
  * monitor, a trusted OS, and/or various statically allocated media buffers.
@@ -397,3 +405,4 @@ ulong board_get_usable_ram_top(ulong total_size)
 {
 	return CONFIG_SYS_SDRAM_BASE + usable_ram_size_below_4g();
 }
+#endif

arch/arm/mach-tegra/tegra210/Makefile
@@ -7,6 +7,8 @@
 
 obj-y	+= clock.o
 obj-y	+= funcmux.o
+obj-y	+= nvtboot_ll.o
+obj-y	+= nvtboot.o
 obj-y	+= pinmux.o
 obj-y	+= xusb-padctl.o
 obj-y	+= ../xusb-padctl-common.o

arch/arm/mach-tegra/tegra210/nvtboot.h
@@ -1,0 +1,16 @@
+/*
+ * nvtboot-related code
+ *
+ * (C) Copyright 2015 NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _NVTBOOT_H_
+#define _NVTBOOT_H_
+
+#include <stdlib.h>
+
+void nvtboot_init(void);
+
+#endif

arch/arm/mach-tegra/tegra210/nvtboot_ll.S
@@ -1,0 +1,20 @@
+/*
+ * Save nvtboot-related boot-time CPU state
+ *
+ * (C) Copyright 2015 NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <config.h>
+#include <linux/linkage.h>
+
+.globl	nvtboot_boot_x5
+nvtboot_boot_x5:
+	.dword 0
+
+ENTRY(save_boot_params)
+	adr	x0, nvtboot_boot_x5
+	str	x5, [x0]
+	b	save_boot_params_ret
+ENDPROC(save_boot_params)

commit d64a95c72563bb2af1fa28af8f9427bf60ff2385

ARM: tegra: fix PCIe RAM usage on Tegra210 with nvtboot
The Tegra PCIe controller can only access RAM below 4G. Configure this
limit so that the PCIe core is aware of it. This mirrors the existing
logic in board2.c, which was used previously before RAM size was queried
from nvtboot.

Bug 200211165

Change-Id: Ib5d2d05d5b8a088a3a62add3465f6f761611ad49
Fixes: 39125874f865 ("ARM: tegra: add support for nvtboot on Tegra210")
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: http://git-master/r/1197691
GVS: Gerrit_Virtual_Submit
Reviewed-by: Tom Warren <twarren@nvidia.com>

arch/arm/mach-tegra/tegra210/nvtboot.c
@@ -315,6 +315,10 @@ void dram_init_banksize(void)
 	gd->bd->bi_dram[0].start = ci->base;
 	gd->bd->bi_dram[0].size = ci->size & ~(SZ_2M - 1);
 
+#ifdef CONFIG_PCI
+	gd->pci_ram_top = gd->bd->bi_dram[0].start + gd->bd->bi_dram[0].size;
+#endif
+
 #ifdef CONFIG_PHYS_64BIT
 	ci = &nvtboot_boot_arg.params.car_info[MEM_LAYOUT_EXTENDED];
 	gd->bd->bi_dram[1].start = ci->base;