5d544940ca6f6d1e7559354d3a66f5c0fb2d4096
Tom Warren <twarren@nvidia.com>
2017-06-13 10:08:54 -0700

    Tegra210: Restore DT delete code & env variables
    In commit aeff514b7876b2791ab9b2b61a6d024e7269d904, I removed both the copy
    and delete fdt code. It has now become apparent that the fdt_del_prop code
    is still needed due to commit 95976c7, which prevents the kernel from
    duplicating pinmux config.
    
    This is basically a revert of aeff514b, and then the removal of just the
    fdt_copy_prop/path code/vars, plus any T186 impact, since it didn't do any
    fdt_del.
    
    Bug 1858109
    
    Change-Id: I3d69d0746382ffd2ea58078e5df3c822a10a3e3d
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: http://git-master/r/1501706

---
+++ arch/arm/mach-tegra/dt-edit.h
@@ -0,0 +1,8 @@
+/*
+ *  Copyright (C) 2010-2017 NVIDIA CORPORATION.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+int fdt_del_env_nodelist(void *blob_dst);
+int fdt_del_env_proplist(void *blob_dst);
