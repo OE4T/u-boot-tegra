OE4T/u-boot-tegra

commit da41cbbdb4a0bc047401e85255163639247b6bb9

ARM: tegra: access PMC registers via monitor
When the PMC is configured to only allow access from secure software,
all PMC register access must be routed through the secure monitor using
SMC calls. This change implements that requirement.

Bug 2064472
Bug 1811649

Change-Id: If03f72530f5e54698520b131eaba062455185c2a
Signed-off-by: kalyani chidambaram <kalyanic@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1668836
(cherry picked from commit 8264f7be8ae5dc7aa1d4f212492f04bedd60fb4e)
Reviewed-on: https://git-master.nvidia.com/r/1693923
(cherry picked from commit 4f89efc534c5043334c01b24f4687b733036cd97 in dev-other-v2016.07)
(swarren, rewrote pmc.c, removed lowlevel_init.S, converted more PMC
register accesses, resolved many small conflicts, rewrote description)
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1741839
Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Tom Warren <twarren@nvidia.com>

include/configs/tegra210-common.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * (C) Copyright 2013-2015
+ * (C) Copyright 2013-2018
  * NVIDIA Corporation <www.nvidia.com>
  */
 
@@ -59,5 +59,8 @@
 
 /* GPU needs setup */
 #define CONFIG_TEGRA_GPU
+
+/* PMC secure access */
+#define CONFIG_ACCESS_PMC_VIA_SMC
 
 #endif /* _TEGRA210_COMMON_H_ */

arch/arm/include/asm/arch-tegra/pmc.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- *  (C) Copyright 2010-2015
+ *  (C) Copyright 2010-2018
  *  NVIDIA Corporation <www.nvidia.com>
  */
 
@@ -387,5 +387,12 @@ struct pmc_ctlr {
 
 /* APBDEV_PMC_CNTRL2_0 0x440 */
 #define HOLD_CKE_LOW_EN				(1 << 12)
+
+/* PMC read/write functions */
+u32 tegra_pmc_readl(unsigned long offset);
+void tegra_pmc_writel(u32 value, unsigned long offset);
+
+/* PMC_CNTRL_0 MAIN_RST*/
+#define MAIN_RST_BIT 4
 
 #endif	/* PMC_H */

arch/arm/include/asm/arch-tegra/tegra.h
@@ -101,8 +101,6 @@ enum {
 	TEGRA_SYSCON_PMC,
 };
 
-#else  /* __ASSEMBLY__ */
-#define PRM_RSTCTRL		NV_PA_PMC_BASE
 #endif
 
 #endif	/* TEGRA_H */

arch/arm/mach-tegra/Makefile
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0+
 #
-# (C) Copyright 2010-2015 Nvidia Corporation.
+# (C) Copyright 2010-2018 Nvidia Corporation.
 #
 # (C) Copyright 2000-2008
 # Wolfgang Denk, DENX Software Engineering, wd@denx.de.
@@ -27,11 +27,11 @@ obj-y += dt-setup.o
 obj-$(CONFIG_TEGRA_CLOCK_SCALING) += emc.o
 obj-$(CONFIG_TEGRA_GPU) += gpu.o
 obj-$(CONFIG_TEGRA_IVC) += ivc.o
-obj-y += lowlevel_init.o
 ifndef CONFIG_SPL_BUILD
 obj-$(CONFIG_ARMV7_PSCI) += psci.o
 endif
 obj-$(CONFIG_DISPLAY_CPUINFO) += sys_info.o
+obj-y += pmc.o
 
 obj-$(CONFIG_TEGRA20) += tegra20/
 obj-$(CONFIG_TEGRA30) += tegra30/

arch/arm/mach-tegra/ap.c
@@ -150,8 +150,7 @@ static u32 get_odmdata(void)
 
 static void init_pmc_scratch(void)
 {
-	struct pmc_ctlr *const pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
-	u32 odmdata;
+	u32 odmdata, ofs;
 	int i;
 
 	/* SCRATCH0 is initialized by the boot ROM and shouldn't be cleared */
@@ -159,13 +158,15 @@ static void init_pmc_scratch(void)
 	if (!tegra_cpu_is_non_secure())
 #endif
 	{
+		ofs = offsetof(struct pmc_ctlr, pmc_scratch1);
 		for (i = 0; i < 23; i++)
-			writel(0, &pmc->pmc_scratch1+i);
+			tegra_pmc_writel(0, ofs + (i * 4));
 	}
 
 	/* ODMDATA is for kernel use to determine RAM size, LP config, etc. */
 	odmdata = get_odmdata();
-	writel(odmdata, &pmc->pmc_scratch20);
+	ofs = offsetof(struct pmc_ctlr, pmc_scratch20);
+	tegra_pmc_writel(odmdata, ofs);
 }
 
 #ifdef CONFIG_ARMV7_SECURE_RESERVE_SIZE

arch/arm/mach-tegra/clock.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2010-2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2010-2018, NVIDIA CORPORATION.  All rights reserved.
  */
 
 /* Tegra SoC common clock control functions */
@@ -814,11 +814,16 @@ void tegra30_set_up_pllp(void)
 
 int clock_external_output(int clk_id)
 {
-	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
+	u32 val;
 
 	if (clk_id >= 1 && clk_id <= 3) {
-		setbits_le32(&pmc->pmc_clk_out_cntrl,
-			     1 << (2 + (clk_id - 1) * 8));
+		val = tegra_pmc_readl(offsetof(struct pmc_ctlr,
+				      pmc_clk_out_cntrl));
+		val |= 1 << (2 + (clk_id - 1) * 8);
+		tegra_pmc_writel(val,
+				 offsetof(struct pmc_ctlr,
+				 pmc_clk_out_cntrl));
+
 	} else {
 		printf("%s: Unknown output clock id %d\n", __func__, clk_id);
 		return -EINVAL;

arch/arm/mach-tegra/cmd_enterrcm.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * Derived from code (arch/arm/lib/reset.c) that is:
  *
@@ -31,13 +31,14 @@
 static int do_enterrcm(cmd_tbl_t *cmdtp, int flag, int argc,
 		       char * const argv[])
 {
-	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
-
 	puts("Entering RCM...\n");
 	udelay(50000);
 
-	pmc->pmc_scratch0 = 2;
+	tegra_pmc_writel(2, offsetof(struct pmc_ctlr, pmc_scratch0));
+
 	disable_interrupts();
+
+	/* reset_cpu */
 	reset_cpu(0);
 
 	return 0;

arch/arm/mach-tegra/cpu.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2010-2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2010-2018, NVIDIA CORPORATION.  All rights reserved.
  */
 
 #include <common.h>
@@ -299,21 +299,19 @@ void enable_cpu_clock(int enable)
 
 static int is_cpu_powered(void)
 {
-	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
-
-	return (readl(&pmc->pmc_pwrgate_status) & CPU_PWRED) ? 1 : 0;
+	return (tegra_pmc_readl(offsetof(struct pmc_ctlr,
+				pmc_pwrgate_status)) & CPU_PWRED) ? 1 : 0;
 }
 
 static void remove_cpu_io_clamps(void)
 {
-	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
 	u32 reg;
 	debug("%s entry\n", __func__);
 
 	/* Remove the clamps on the CPU I/O signals */
-	reg = readl(&pmc->pmc_remove_clamping);
+	reg = tegra_pmc_readl(offsetof(struct pmc_ctlr, pmc_remove_clamping));
 	reg |= CPU_CLMP;
-	writel(reg, &pmc->pmc_remove_clamping);
+	tegra_pmc_writel(reg, offsetof(struct pmc_ctlr, pmc_remove_clamping));
 
 	/* Give I/O signals time to stabilize */
 	udelay(IO_STABILIZATION_DELAY);
@@ -321,17 +319,19 @@ static void remove_cpu_io_clamps(void)
 
 void powerup_cpu(void)
 {
-	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
 	u32 reg;
 	int timeout = IO_STABILIZATION_DELAY;
 	debug("%s entry\n", __func__);
 
 	if (!is_cpu_powered()) {
 		/* Toggle the CPU power state (OFF -> ON) */
-		reg = readl(&pmc->pmc_pwrgate_toggle);
+		reg = tegra_pmc_readl(offsetof(struct pmc_ctlr,
+				      pmc_pwrgate_toggle));
 		reg &= PARTID_CP;
 		reg |= START_CP;
-		writel(reg, &pmc->pmc_pwrgate_toggle);
+		tegra_pmc_writel(reg,
+				 offsetof(struct pmc_ctlr,
+				 pmc_pwrgate_toggle));
 
 		/* Wait for the power to come up */
 		while (!is_cpu_powered()) {

commit fdac35f8f16ac7dc5d577515fc95748d0ad2bb63

ARM: tegra210: don't define CONFIG_ACCESS_PMC_VIA_SMC
This config option isn't used, and was accidentally introduced as part of
a cherry-pick that was originally written for T210 and not fully converted
for T124.

Bug 2064472

Change-Id: I8598efb2cdf14de0e47d6b424bc3c1eac27b261e
Fixes: da41cbbdb4a0 "ARM: tegra: access PMC registers via monitor"
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1754924
GVS: Gerrit_Virtual_Submit
Reviewed-by: Tom Warren <twarren@nvidia.com>

include/configs/tegra210-common.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * (C) Copyright 2013-2018
+ * (C) Copyright 2013-2015
  * NVIDIA Corporation <www.nvidia.com>
  */
 
@@ -59,8 +59,5 @@
 
 /* GPU needs setup */
 #define CONFIG_TEGRA_GPU
-
-/* PMC secure access */
-#define CONFIG_ACCESS_PMC_VIA_SMC
 
 #endif /* _TEGRA210_COMMON_H_ */