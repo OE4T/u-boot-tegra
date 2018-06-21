OE4T/u-boot-tegra

commit fba67d87d1c6cf3ef9d50ed5f56bb939d8439c0c

ARM: tegra: add warmboot support for Tegra124
Bug 2064472

Change-Id: I42af29fd6ab7766f555069a1b3d0a7fec21581ea
Signed-off-by: Bryan Wu <pengw@nvidia.com>
(cherry picked from commit 21de12a966e52bbe86e91e3ba1df44e398e0ed8c in l4t/l4t-r21 v2014.04-rc2)
(swarren, squashed in another patch:)
"warmboot: change wb prepare code for t1x4"
Signed-off-by: Yen Lin <yelin@nvidia.com>
Reviewed-on: https://git-master/r/1503314
(cherry picked from commit b1d8a758cd4c5d77988d1a75a78900a296caebed in l4t/l4t-r21 v2014.04-rc2)
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1703497
GVS: Gerrit_Virtual_Submit
Reviewed-by: Winnie Hsu <whsu@nvidia.com>

arch/arm/include/asm/arch-tegra/warmboot.h
@@ -7,11 +7,15 @@
 #ifndef _WARM_BOOT_H_
 #define _WARM_BOOT_H_
 
+#include <asm/arch-tegra/sdram_param.h>
+
 #define STRAP_OPT_A_RAM_CODE_SHIFT	4
 #define STRAP_OPT_A_RAM_CODE_MASK	(0xf << STRAP_OPT_A_RAM_CODE_SHIFT)
 
 /* Defines the supported operating modes */
 enum fuse_operating_mode {
+	MODE_ODM_PRODUCTION = 1,
+	MODE_PREPRODUCTION = 2,
 	MODE_PRODUCTION = 3,
 	MODE_UNDEFINED,
 };
@@ -38,13 +42,23 @@ struct hash {
 struct wb_header {
 	u32 length_insecure;	/* length of the code header */
 	u32 reserved[3];
+#ifdef CONFIG_TEGRA124
+	u32 modules[64];	/* RsaKeyModulus */
+	struct {
+		struct hash hash;  /* hash of header+code, starts next field */
+		u32 signature[64]; /* The RSA-PSS signature*/
+	} hash_signature;
+#else
 	struct hash hash;	/* hash of header+code, starts next field*/
+#endif
 	struct hash random_aes_block;	/* a data block to aid security. */
 	u32 length_secure;	/* length of the code header */
 	u32 destination;	/* destination address to put the wb code */
 	u32 entry_point;	/* execution address of the wb code */
 	u32 code_length;	/* length of the code */
 };
+
+extern struct wb_header wb_header;
 
 /*
  * The warm boot code needs direct access to these registers since it runs in
@@ -117,6 +131,83 @@ union scratch3_reg {
 	u32 word;
 };
 
+enum field_type {
+	TYPE_SDRAM,
+	TYPE_PLLM,
+	TYPE_CONST,
+	TYPE_COUNT,
+};
+
+#define sdram_offset(member) offsetof(struct sdram_params, member)
+#define pmc_offset(member) offsetof(struct pmc_ctlr, member)
+#define pllm_offset(member) offsetof(struct clk_pllm, member)
+
+struct encode_fields {
+	u32 src_offset;
+	u32 op1_mask;
+	u32 op1_shift;
+	u32 op2_mask;
+	u32 op2_shift;
+	u32 dst_offset;
+	u32 dst_shift;
+};
+
+/*
+ * encode: set dst_off.dst_bit to 1 if (src_off.op1_bits > src_off.op2_bits)
+ *         else set dst_off.dst_bit to 0.
+ */
+#define encode(src_off, op1_bits, op2_bits, dst_off, dst_bit)		\
+	{								\
+		.src_offset = src_off,					\
+		.op1_mask = 0xfffffffful >>				\
+			(31 - ((1 ? op1_bits) - (0 ? op1_bits))),	\
+		.op1_shift = 0 ? op1_bits,				\
+		.op2_mask = 0xfffffffful >>				\
+			(31 - ((1 ? op2_bits) - (0 ? op2_bits))),	\
+		.op2_shift = 0 ? op2_bits,				\
+		.dst_offset = dst_off,					\
+		.dst_shift = dst_bit,					\
+	}
+
+struct pack_fields {
+	enum field_type src_type;
+	u32 src_offset;		/* for TYPE_CONST, this field is a constant */
+	u32 src_mask;
+	u32 src_shift;
+	enum field_type dst_type;
+	u32 dst_offset;
+	u32 dst_mask;
+	u32 dst_shift;
+};
+
+/*
+ * pack: a macro to copy from bits in src to bits in dst.
+ *
+ * For example,
+ *   pack(TYPE_SDRAM, sdram_offset(emc_clock_source), 7:0,
+ *	  pmc_offset(pmc_scratch6), 15:8)
+ *     is to:
+ *         copy bits 7:0 of sdram_offset.emc_clock_source to
+ *         bits 15:8 of pmc_scratch6 register.
+ *
+ *   The first parameter of pack determines the type of src:
+ *     TYPE_SDRAM: src is of SDRAM parameter,
+ *     TYPE_PLLM:  src is of PLLM,
+ *     TYPE_CONST: src is a constant.
+ */
+#define pack(_src_type, src_off, src_bits, dst_off, dst_bits)		\
+	{								\
+		.src_type = _src_type,					\
+		.src_offset = src_off,					\
+		.src_mask = 0xfffffffful >>				\
+			(31 - ((1 ? src_bits) - (0 ? src_bits))),	\
+		.src_shift = 0 ? src_bits,				\
+		.dst_offset = dst_off,					\
+		.dst_mask = 0xfffffffful >>				\
+			(31 - ((1 ? dst_bits) - (0 ? dst_bits))),	\
+		.dst_shift = 0 ? dst_bits,				\
+	}
+
 
 /**
  * Save warmboot memory settings for a later resume
@@ -130,4 +221,18 @@ int sign_data_block(u8 *source, u32 length, u8 *signature);
 void wb_start(void);	/* Start of WB assembly code */
 void wb_end(void);	/* End of WB assembly code */
 
+/* Common routines for T114 and T124 SOCs */
+
+/*
+ * t1x4_wb_save_sdram_params():
+ * save sdram parameters to scratch registers so sdram parameters can be
+ * restored when system resumes from LP0
+ */
+int t1x4_wb_save_sdram_params(struct sdram_params *sdram);
+
+/*
+ * t1x4_wb_prepare_code():
+ * prepare WB code, which will be executed by AVP when system resumes from LP0
+ */
+int t1x4_wb_prepare_code(u32 tegra_id, u32 seg_address, u32 seg_length);
 #endif

arch/arm/include/asm/arch-tegra/ap.h
@@ -25,6 +25,7 @@
 
 /* AP base physical address of internal SRAM */
 #define NV_PA_BASE_SRAM		0x40000000
+#define NV_PA_BASE_SRAM_SIZE    0x20000
 
 #define EXCEP_VECTOR_CPU_RESET_VECTOR	(NV_PA_EVP_BASE + 0x100)
 #define CSITE_CPU_DBG0_LAR		(NV_PA_CSITE_BASE + 0x10FB0)

arch/arm/include/asm/arch-tegra/clk_rst.h
@@ -229,12 +229,43 @@ struct clk_rst_ctlr {
 	uint crc_clk_src_y[TEGRA_CLK_SOURCES_Y]; /* SPARE1, etc, 0x694-0x6D8 */
 };
 
+#define TEGRA_DEV_L			0
+#define TEGRA_DEV_H			1
+#define TEGRA_DEV_U			2
+#define TEGRA_DEV_V			0
+#define TEGRA_DEV_W			1
+
+#define SIMPLE_PLLX		(CLOCK_ID_XCPU - CLOCK_ID_FIRST_SIMPLE)
+
+/* Bits to enable/reset modules */
+#define CLK_ENB_CPU			(1 << 0)
+#define SWR_TRIG_SYS_RST		(1 << 2)
+#define SWR_CSITE_RST			(1 << 9)
+#define CLK_ENB_CSITE			(1 << 9)
+
+/* CRC_SUPER_CCLK_DIVIDER_0 0x24 */
+#define SUPER_CDIV_ENB_ENABLE		(1 << 31)
+
+/* CLK_RST_CONTROLLER_MISC_CLK_ENB 0x48 */
+#define EN_PPSB_STOPCLK			(1 << 0)
+
 /* CLK_RST_CONTROLLER_CLK_CPU_CMPLX_0 */
 #define CPU3_CLK_STP_SHIFT	11
 #define CPU2_CLK_STP_SHIFT	10
 #define CPU1_CLK_STP_SHIFT	9
 #define CPU0_CLK_STP_SHIFT	8
 #define CPU0_CLK_STP_MASK	(1U << CPU0_CLK_STP_SHIFT)
+
+/* CRC_OSC_CTRL_0 0x50 */
+#define OSC_CTRL_OSC_FREQ		(0xf << 28)
+#define OSC_CTRL_OSC_FREQ_SHIFT		28
+#define OSC_FREQ_OSC13			0	/* 13.0MHz */
+#define OSC_FREQ_OSC19P2		4	/* 19.2MHz */
+#define OSC_FREQ_OSC12			8	/* 12.0MHz */
+#define OSC_FREQ_OSC26			12	/* 26.0MHz */
+#define OSC_FREQ_OSC16P8		1	/* 16.8MHz */
+#define OSC_FREQ_OSC38P4		5	/* 38.4MHz */
+#define OSC_FREQ_OSC48			9	/* 48.0MHz */
 
 /* CLK_RST_CONTROLLER_PLLx_BASE_0 */
 #define PLL_BYPASS_SHIFT	31
@@ -289,6 +320,26 @@ enum {
 #define PLLP_OUT4_RSTN_DIS	(1 << 16)
 #define PLLP_OUT4_RSTN_EN	(0 << 16)
 #define PLLP_OUT4_CLKEN		(1 << 17)
+
+/* CRC_PLLP_MISC_0 0xac */
+#define PLLP_MISC_PLLP_CPCON_8		(8 << 8)
+#define PLLP_MISC_PLLP_LOCK_ENABLE	(1 << 18)
+
+/* CRC_PLLU_BASE_0 0xc0 */
+#define PLLU_BYPASS_ENABLE		(1 << 31)
+#define PLLU_ENABLE_ENABLE		(1 << 30)
+#define PLLU_REF_DIS_REF_DISABLE	(1 << 29)
+#define PLLU_OVERRIDE_ENABLE		(1 << 24)
+
+/* CRC_PLLU_MISC_0 0xcc */
+#define PLLU_LOCK_ENABLE_ENABLE		(1 << 22)
+
+/* PLLX_BASE_0 0xe0 */
+#define PLLX_BASE_PLLX_ENABLE		(1 << 30)
+
+/* CLK_RST_CONTROLLER_PLLX_MISC_3 */
+#define PLLX_IDDQ_SHIFT			3
+#define PLLX_IDDQ_MASK			(1U << PLLX_IDDQ_SHIFT)
 
 /* CLK_RST_CONTROLLER_UTMIP_PLL_CFG1_0 */
 #define PLLU_POWERDOWN		(1 << 16)

arch/arm/include/asm/arch-tegra/sdram_param.h
@@ -1,0 +1,344 @@
+/*
+ * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TEGRA124_SDRAM_PARAM_H_
+#define _TEGRA124_SDRAM_PARAM_H_
+
+/* define SDRAM parameters offset in BCT */
+#define BCT_SDRAM_PARAMS_OFFSET	0x8f8
+#define SDRAM_PARAMS_BASE	(NV_PA_BASE_SRAM + BCT_SDRAM_PARAMS_OFFSET)
+
+enum memory_type {
+	MEMORY_TYPE_NONE = 0,
+	MEMORY_TYPE_LPDDR2,
+	MEMORY_TYPE_DDR3,
+};
+
+/**
+ * Defines the SDRAM parameter structure
+ */
+struct sdram_params {
+	enum memory_type memory_type;
+	u32 pllm_input_divider;
+	u32 pllm_feedback_divider;
+	u32 pllm_stable_time;
+	u32 pllm_setup_control;
+	u32 pllm_select_div2;
+	u32 pllm_pdl_shift_ph45;
+	u32 pllm_pdl_shift_ph90;
+	u32 pllm_pdl_shift_ph135;
+	u32 pllm_kcp;
+	u32 pllm_kvco;
+	u32 emc_bct_spare0;
+	u32 emc_bct_spare1;
+	u32 emc_bct_spare2;
+	u32 emc_bct_spare3;
+	u32 emc_bct_spare4;
+	u32 emc_bct_spare5;
+	u32 emc_bct_spare6;
+	u32 emc_bct_spare7;
+	u32 emc_bct_spare8;
+	u32 emc_bct_spare9;
+	u32 emc_bct_spare10;
+	u32 emc_bct_spare11;
+	u32 emc_clock_source;
+	u32 emc_auto_cal_interval;
+	u32 emc_auto_cal_config;
+	u32 emc_auto_cal_config2;
+	u32 emc_auto_cal_config3;
+	u32 emc_auto_cal_wait;
+	u32 emc_adr_cfg;
+	u32 emc_pin_program_wait;
+	u32 emc_pin_extra_wait;
+	u32 emc_timing_control_wait;
+	u32 emc_rc;
+	u32 emc_rfc;
+	u32 emc_rfc_slr;
+	u32 emc_ras;
+	u32 emc_rp;
+	u32 emc_r2r;
+	u32 emc_w2w;
+	u32 emc_r2w;
+	u32 emc_w2r;
+	u32 emc_r2p;
+	u32 emc_w2p;
+	u32 emc_rd_rcd;
+	u32 emc_wr_rcd;
+	u32 emc_rdr;
+	u32 emc_rext;
+	u32 emc_wext;
+	u32 emc_wdv;
+	u32 emc_wdv_mask;
+	u32 emc_quse;
+	u32 emc_quse_width;
+	u32 emc_ibdly;
+	u32 emc_einput;
+	u32 emc_einput_duration;
+	u32 emc_puterm_extra;
+	u32 emc_puterm_width;
+	u32 emc_puterm_adj;
+	u32 emc_cdb_cntl1;
+	u32 emc_cdb_cntl2;
+	u32 emc_cdb_cntl3;
+	u32 emc_qrst;
+	u32 emc_qsafe;
+	u32 emc_rdv;
+	u32 emc_rdv_mask;
+	u32 emc_qpop;
+	u32 emc_ctt;
+	u32 emc_ctt_duration;
+	u32 emc_refresh;
+	u32 emc_burst_refresh_num;
+	u32 emc_pre_refresh_req_cnt;
+	u32 emc_pdex2wr;
+	u32 emc_pdex2rd;
+	u32 emc_pchg2pden;
+	u32 emc_act2pden;
+	u32 emc_ar2pden;
+	u32 emc_rw2pden;
+	u32 emc_txsr;
+	u32 emc_txsrdll;
+	u32 emc_tcke;
+	u32 emc_tckesr;
+	u32 emc_tpd;
+	u32 emc_tfaw;
+	u32 emc_trpab;
+	u32 emc_tclkstable;
+	u32 emc_tclkstop;
+	u32 emc_trefbw;
+	u32 emc_fbio_cfg5;
+	u32 emc_fbio_cfg6;
+	u32 emc_fbio_spare;
+	u32 emc_cfg_rsv;
+	u32 emc_mrs;
+	u32 emc_emrs;
+	u32 emc_emrs2;
+	u32 emc_emrs3;
+	u32 emc_mrw1;
+	u32 emc_mrw2;
+	u32 emc_mrw3;
+	u32 emc_mrw4;
+	u32 emc_mrw_extra;
+	u32 emc_warmboot_mrw_extra;
+	u32 emc_warmboot_extra_modereg_write_en;
+	u32 emc_extra_modereg_write_enable;
+	u32 emc_mrw_reset_command;
+	u32 emc_mrw_reset_ninit_wait;
+	u32 emc_mrs_wait_cnt;
+	u32 emc_mrs_wait_cnt2;
+	u32 emc_cfg;
+	u32 emc_cfg2;
+	u32 emc_cfg_pipe;
+	u32 emc_dbg;
+	u32 emc_cmdq;
+	u32 emc_mc2emcq;
+	u32 emc_dyn_self_ref_control;
+	u32 ahb_arb_xbar_ctrl_mem_init_done;
+	u32 emc_cfg_dig_dll;
+	u32 emc_cfg_dig_dll_period;
+	u32 emc_dev_select;
+	u32 emc_sel_dpd_ctrl;
+	u32 emc_dll_xform_dqs0;
+	u32 emc_dll_xform_dqs1;
+	u32 emc_dll_xform_dqs2;
+	u32 emc_dll_xform_dqs3;
+	u32 emc_dll_xform_dqs4;
+	u32 emc_dll_xform_dqs5;
+	u32 emc_dll_xform_dqs6;
+	u32 emc_dll_xform_dqs7;
+	u32 emc_dll_xform_dqs8;
+	u32 emc_dll_xform_dqs9;
+	u32 emc_dll_xform_dqs10;
+	u32 emc_dll_xform_dqs11;
+	u32 emc_dll_xform_dqs12;
+	u32 emc_dll_xform_dqs13;
+	u32 emc_dll_xform_dqs14;
+	u32 emc_dll_xform_dqs15;
+	u32 emc_dll_xform_quse0;
+	u32 emc_dll_xform_quse1;
+	u32 emc_dll_xform_quse2;
+	u32 emc_dll_xform_quse3;
+	u32 emc_dll_xform_quse4;
+	u32 emc_dll_xform_quse5;
+	u32 emc_dll_xform_quse6;
+	u32 emc_dll_xform_quse7;
+	u32 emc_dll_xform_addr0;
+	u32 emc_dll_xform_addr1;
+	u32 emc_dll_xform_addr2;
+	u32 emc_dll_xform_addr3;
+	u32 emc_dll_xform_addr4;
+	u32 emc_dll_xform_addr5;
+	u32 emc_dll_xform_quse8;
+	u32 emc_dll_xform_quse9;
+	u32 emc_dll_xform_quse10;
+	u32 emc_dll_xform_quse11;
+	u32 emc_dll_xform_quse12;
+	u32 emc_dll_xform_quse13;
+	u32 emc_dll_xform_quse14;
+	u32 emc_dll_xform_quse15;
+	u32 emc_dli_trim_txdqs0;
+	u32 emc_dli_trim_txdqs1;
+	u32 emc_dli_trim_txdqs2;
+	u32 emc_dli_trim_txdqs3;
+	u32 emc_dli_trim_txdqs4;
+	u32 emc_dli_trim_txdqs5;
+	u32 emc_dli_trim_txdqs6;
+	u32 emc_dli_trim_txdqs7;
+	u32 emc_dli_trim_txdqs8;
+	u32 emc_dli_trim_txdqs9;
+	u32 emc_dli_trim_txdqs10;
+	u32 emc_dli_trim_txdqs11;
+	u32 emc_dli_trim_txdqs12;
+	u32 emc_dli_trim_txdqs13;
+	u32 emc_dli_trim_txdqs14;
+	u32 emc_dli_trim_txdqs15;
+	u32 emc_dll_xform_dq0;
+	u32 emc_dll_xform_dq1;
+	u32 emc_dll_xform_dq2;
+	u32 emc_dll_xform_dq3;
+	u32 emc_dll_xform_dq4;
+	u32 emc_dll_xform_dq5;
+	u32 emc_dll_xform_dq6;
+	u32 emc_dll_xform_dq7;
+	u32 warmboot_wait;
+	u32 emc_ctt_term_ctrl;
+	u32 emc_odt_write;
+	u32 emc_odt_read;
+	u32 emc_zcal_interval;
+	u32 emc_zcal_wait_cnt;
+	u32 emc_zcal_mrw_cmd;
+	u32 emc_mrs_rest_dll;
+	u32 emc_zcal_init_dev0;
+	u32 emc_zcal_init_dev1;
+	u32 emc_zcal_init_wait;
+	u32 emc_zcal_warmcoldboot_enables;
+	u32 emc_mrw_lpddr2_zcal_warmboot;
+	u32 emc_zq_cal_ddr3_warmboot;
+	u32 emc_zcal_warmboot_wait;
+	u32 emc_mrs_warmboot_enable;
+	u32 emc_mrs_reset_dll_wait;
+	u32 emc_mrs_extra;
+	u32 emc_warmboot_mrs_extra;
+	u32 emc_emrs_ddr2_dll_enable;
+	u32 emc_mrs_ddr2_dll_reset;
+	u32 emc_emrs_ddr2_ocd_calib;
+	u32 emc_ddr2_wait;
+	u32 emc_clken_override;
+	u32 mc_dis_extra_snap_levels;
+	u32 emc_extra_refresh_num;
+	u32 emc_clken_override_all_warmboot;
+	u32 mc_clken_override_all_warmboot;
+	u32 emc_cfg_dig_dll_period_warmboot;
+	u32 pmc_vddp_sel;
+	u32 pmc_vddp_sel_wait;
+	u32 pmc_ddr_pwr;
+	u32 pmc_ddr_cfg;
+	u32 pmc_io_dpd3_req;
+	u32 pmc_io_dpd3_req_wait;
+	u32 pmc_reg_short;
+	u32 pmc_no_iopower;
+	u32 pmc_por_dpd_ctrl_wait;
+	u32 emc_xm2cmdpadctrl;
+	u32 emc_xm2cmdpadctrl2;
+	u32 emc_xm2cmdpadctrl3;
+	u32 emc_xm2cmdpadctrl4;
+	u32 emc_xm2cmdpadctrl5;
+	u32 emc_xm2dqspadctrl;
+	u32 emc_xm2dqspadctrl2;
+	u32 emc_xm2dqspadctrl3;
+	u32 emc_xm2dqspadctrl4;
+	u32 emc_xm2dqspadctrl5;
+	u32 emc_xm2dqspadctrl6;
+	u32 emc_xm2dqpadctrl;
+	u32 emc_xm2dqpadctrl2;
+	u32 emc_xm2dqpadctrl3;
+	u32 emc_xm2clkpadctrl;
+	u32 emc_xm2clkpadctrl2;
+	u32 emc_xm2comppadctrl;
+	u32 emc_xm2vttgenpadctrl;
+	u32 emc_xm2vttgenpadctrl2;
+	u32 emc_xm2vttgenpadctrl3;
+	u32 emc_acpd_control;
+	u32 emc_swizzle_rank0_byte_cfg;
+	u32 emc_swizzle_rank0_byte0;
+	u32 emc_swizzle_rank0_byte1;
+	u32 emc_swizzle_rank0_byte2;
+	u32 emc_swizzle_rank0_byte3;
+	u32 emc_swizzle_rank1_byte_cfg;
+	u32 emc_swizzle_rank1_byte0;
+	u32 emc_swizzle_rank1_byte1;
+	u32 emc_swizzle_rank1_byte2;
+	u32 emc_swizzle_rank1_byte3;
+	u32 emc_dsr_vttgen_drv;
+	u32 emc_txdsrvttgen;
+	u32 emc_bgbias_ctl0;
+	u32 mc_emem_adr_cfg;
+	u32 mc_emem_adr_cfg_dev0;
+	u32 mc_emem_adr_cfg_dev1;
+	u32 mc_emem_adr_cfg_bank_mask0;
+	u32 mc_emem_adr_cfg_bank_mask1;
+	u32 mc_emem_adr_cfg_bank_mask2;
+	u32 mc_emem_adr_cfg_bank_swizzle3;
+	u32 mc_emem_cfg;
+	u32 mc_emem_arb_cfg;
+	u32 mc_emem_arb_outstanding_req;
+	u32 mc_emem_arb_timing_rcd;
+	u32 mc_emem_arb_timing_rp;
+	u32 mc_emem_arb_timing_rc;
+	u32 mc_emem_arb_timing_ras;
+	u32 mc_emem_arb_timing_faw;
+	u32 mc_emem_arb_timing_rrd;
+	u32 mc_emem_arb_timing_rap2pre;
+	u32 mc_emem_arb_timing_wap2pre;
+	u32 mc_emem_arb_timing_r2r;
+	u32 mc_emem_arb_timing_w2w;
+	u32 mc_emem_arb_timing_r2w;
+	u32 mc_emem_arb_timing_w2r;
+	u32 mc_emem_arb_da_turns;
+	u32 mc_emem_arb_da_covers;
+	u32 mc_emem_arb_misc0;
+	u32 mc_emem_arb_misc1;
+	u32 mc_emem_arb_ring1_throttle;
+	u32 mc_emem_arb_override;
+	u32 mc_emem_arb_override1;
+	u32 mc_emem_arb_rsv;
+	u32 mc_clken_override;
+	u32 mc_stat_control;
+	u32 mc_display_snap_ring;
+	u32 mc_video_protect_bom;
+	u32 mc_video_protect_bom_adr_hi;
+	u32 mc_video_protect_size_mb;
+	u32 mc_video_protect_vpr_override;
+	u32 mc_video_protect_vpr_override1;
+	u32 mc_video_protect_gpu_override0;
+	u32 mc_video_protect_gpu_override1;
+	u32 mc_sec_carveout_bom;
+	u32 mc_sec_carveout_adr_hi;
+	u32 mc_sec_carveout_size_mb;
+	u32 mc_video_protect_write_access;
+	u32 mc_sec_carveout_protect_write_access;
+	u32 emc_ca_training_enable;
+	u32 emc_ca_training_timing_cntl1;
+	u32 emc_ca_training_timing_cntl2;
+	u32 swizzle_rank_byte_encode;
+	u32 bootrom_patch_control;
+	u32 bootrom_patch_data;
+	u32 mc_mts_carveout_bom;
+	u32 mc_mts_carveout_adr_hi;
+	u32 mc_mts_carveout_size_mb;
+	u32 mc_mts_carveout_reg_ctrl;
+};
+#endif	/* _TEGRA124_SDRAM_PARAM_H_ */

arch/arm/mach-tegra/board2.c
@@ -165,9 +165,6 @@ int board_init(void)
 #ifdef CONFIG_TEGRA_LP0
 	/* save Sdram params to PMC 2, 4, and 24 for WB0 */
 	warmboot_save_sdram_params();
-
-	/* prepare the WB code to LP0 location */
-	warmboot_prepare_code(TEGRA_LP0_ADDR, TEGRA_LP0_SIZE);
 #endif
 	return nvidia_board_init();
 }
@@ -220,6 +217,11 @@ int board_late_init(void)
 	}
 #endif
 	start_cpu_fan();
+
+#ifdef CONFIG_TEGRA_LP0
+	/* prepare the WB code to LP0 location */
+	warmboot_prepare_code(TEGRA_LP0_ADDR, TEGRA_LP0_SIZE);
+#endif
 
 	return 0;
 }

arch/arm/mach-tegra/tegra124/Makefile
@@ -17,3 +17,13 @@ obj-y	+= ../xusb-padctl-common.o
 ifndef CONFIG_SPL_BUILD
 obj-$(CONFIG_ARMV7_NONSEC) += psci.o
 endif
+
+# The AVP is ARMv4T architecture so we must use special compiler
+# flags for any startup files it might use.
+CFLAGS_warmboot_avp.o = -march=armv4t -U__LINUX_ARM_ARCH__ \
+        -D__LINUX_ARM_ARCH__=4
+
+obj-$(CONFIG_TEGRA_LP0) += crypto.o
+obj-$(CONFIG_TEGRA_LP0) += t1x4_warmboot.o
+obj-$(CONFIG_TEGRA_LP0) += warmboot.o
+obj-$(CONFIG_TEGRA_LP0) += warmboot_avp.o

arch/arm/mach-tegra/tegra124/crypto.c
@@ -1,0 +1,230 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors.
+ * (C) Copyright 2010 - 2011 NVIDIA Corporation <www.nvidia.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <errno.h>
+#include <uboot_aes.h>
+#include "crypto.h"
+
+static u8 zero_key[16];
+
+#define AES_CMAC_CONST_RB 0x87  /* from RFC 4493, Figure 2.2 */
+
+enum security_op {
+	SECURITY_SIGN		= 1 << 0,	/* Sign the data */
+	SECURITY_ENCRYPT	= 1 << 1,	/* Encrypt the data */
+};
+
+static void debug_print_vector(char *name, u32 num_bytes, u8 *data)
+{
+	u32 i;
+
+	debug("%s [%d] @0x%08x", name, num_bytes, (u32)data);
+	for (i = 0; i < num_bytes; i++) {
+		if (i % 16 == 0)
+			debug(" = ");
+		debug("%02x", data[i]);
+		if ((i+1) % 16 != 0)
+			debug(" ");
+	}
+	debug("\n");
+}
+
+/**
+ * Apply chain data to the destination using EOR
+ *
+ * Each array is of length AES_AES_KEY_LENGTH.
+ *
+ * \param cbc_chain_data	Chain data
+ * \param src			Source data
+ * \param dst			Destination data, which is modified here
+ */
+static void apply_cbc_chain_data(u8 *cbc_chain_data, u8 *src, u8 *dst)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+		*dst++ = *src++ ^ *cbc_chain_data++;
+}
+
+/**
+ * Encrypt some data with AES.
+ *
+ * \param key_schedule		Expanded key to use
+ * \param src			Source data to encrypt
+ * \param dst			Destination buffer
+ * \param num_aes_blocks	Number of AES blocks to encrypt
+ */
+static void encrypt_object(u8 *key_schedule, u8 *src, u8 *dst,
+			   u32 num_aes_blocks)
+{
+	u8 tmp_data[AES_KEY_LENGTH];
+	u8 *cbc_chain_data;
+	u32 i;
+
+	cbc_chain_data = zero_key;	/* Convenient array of 0's for IV */
+
+	for (i = 0; i < num_aes_blocks; i++) {
+		debug("encrypt_object: block %d of %d\n", i, num_aes_blocks);
+		debug_print_vector("AES Src", AES_KEY_LENGTH, src);
+
+		/* Apply the chain data */
+		apply_cbc_chain_data(cbc_chain_data, src, tmp_data);
+		debug_print_vector("AES Xor", AES_KEY_LENGTH, tmp_data);
+
+		/* encrypt the AES block */
+		aes_encrypt(tmp_data, key_schedule, dst);
+		debug_print_vector("AES Dst", AES_KEY_LENGTH, dst);
+
+		/* Update pointers for next loop. */
+		cbc_chain_data = dst;
+		src += AES_KEY_LENGTH;
+		dst += AES_KEY_LENGTH;
+	}
+}
+
+/**
+ * Shift a vector left by one bit
+ *
+ * \param in	Input vector
+ * \param out	Output vector
+ * \param size	Length of vector in bytes
+ */
+static void left_shift_vector(u8 *in, u8 *out, int size)
+{
+	int carry = 0;
+	int i;
+
+	for (i = size - 1; i >= 0; i--) {
+		out[i] = (in[i] << 1) | carry;
+		carry = in[i] >> 7;	/* get most significant bit */
+	}
+}
+
+/**
+ * Sign a block of data, putting the result into dst.
+ *
+ * \param key			Input AES key, length AES_KEY_LENGTH
+ * \param key_schedule		Expanded key to use
+ * \param src			Source data of length 'num_aes_blocks' blocks
+ * \param dst			Destination buffer, length AES_KEY_LENGTH
+ * \param num_aes_blocks	Number of AES blocks to encrypt
+ */
+static void sign_object(u8 *key, u8 *key_schedule, u8 *src, u8 *dst,
+			u32 num_aes_blocks)
+{
+	u8 tmp_data[AES_KEY_LENGTH];
+	u8 left[AES_KEY_LENGTH];
+	u8 k1[AES_KEY_LENGTH];
+	u8 *cbc_chain_data;
+	unsigned i;
+
+	cbc_chain_data = zero_key;	/* Convenient array of 0's for IV */
+
+	/* compute K1 constant needed by AES-CMAC calculation */
+	for (i = 0; i < AES_KEY_LENGTH; i++)
+		tmp_data[i] = 0;
+
+	encrypt_object(key_schedule, tmp_data, left, 1);
+	debug_print_vector("AES(key, nonce)", AES_KEY_LENGTH, left);
+
+	left_shift_vector(left, k1, sizeof(left));
+	debug_print_vector("L", AES_KEY_LENGTH, left);
+
+	if ((left[0] >> 7) != 0) /* get MSB of L */
+		k1[AES_KEY_LENGTH-1] ^= AES_CMAC_CONST_RB;
+	debug_print_vector("K1", AES_KEY_LENGTH, k1);
+
+	/* compute the AES-CMAC value */
+	for (i = 0; i < num_aes_blocks; i++) {
+		/* Apply the chain data */
+		apply_cbc_chain_data(cbc_chain_data, src, tmp_data);
+
+		/* for the final block, XOR K1 into the IV */
+		if (i == num_aes_blocks - 1)
+			apply_cbc_chain_data(tmp_data, k1, tmp_data);
+
+		/* encrypt the AES block */
+		aes_encrypt(tmp_data, key_schedule, dst);
+
+		debug("sign_obj: block %d of %d\n", i, num_aes_blocks);
+		debug_print_vector("AES-CMAC Src", AES_KEY_LENGTH, src);
+		debug_print_vector("AES-CMAC Xor", AES_KEY_LENGTH, tmp_data);
+		debug_print_vector("AES-CMAC Dst", AES_KEY_LENGTH, dst);
+
+		/* Update pointers for next loop. */
+		cbc_chain_data = dst;
+		src += AES_KEY_LENGTH;
+	}
+
+	debug_print_vector("AES-CMAC Hash", AES_KEY_LENGTH, dst);
+}
+
+/**
+ * Encrypt and sign a block of data (depending on security mode).
+ *
+ * \param key		Input AES key, length AES_KEY_LENGTH
+ * \param oper		Security operations mask to perform (enum security_op)
+ * \param src		Source data
+ * \param length	Size of source data
+ * \param sig_dst	Destination address for signature, AES_KEY_LENGTH bytes
+ */
+static int encrypt_and_sign(u8 *key, enum security_op oper, u8 *src,
+			    u32 length, u8 *sig_dst)
+{
+	u32 num_aes_blocks;
+	u8 key_schedule[AES_EXPAND_KEY_LENGTH];
+
+	debug("encrypt_and_sign: length = %d\n", length);
+	debug_print_vector("AES key", AES_KEY_LENGTH, key);
+
+	/*
+	 * The only need for a key is for signing/checksum purposes, so
+	 * if not encrypting, expand a key of 0s.
+	 */
+	aes_expand_key(oper & SECURITY_ENCRYPT ? key : zero_key, key_schedule);
+
+	num_aes_blocks = (length + AES_KEY_LENGTH - 1) / AES_KEY_LENGTH;
+
+	if (oper & SECURITY_ENCRYPT) {
+		/* Perform this in place, resulting in src being encrypted. */
+		debug("encrypt_and_sign: begin encryption\n");
+		encrypt_object(key_schedule, src, src, num_aes_blocks);
+		debug("encrypt_and_sign: end encryption\n");
+	}
+
+	if (oper & SECURITY_SIGN) {
+		/* encrypt the data, overwriting the result in signature. */
+		debug("encrypt_and_sign: begin signing\n");
+		sign_object(key, key_schedule, src, sig_dst, num_aes_blocks);
+		debug("encrypt_and_sign: end signing\n");
+	}
+
+	return 0;
+}
+
+int sign_data_block(u8 *source, unsigned length, u8 *signature)
+{
+	return encrypt_and_sign(zero_key, SECURITY_SIGN, source,
+				length, signature);
+}

commit ff1b3966c8780b2513878ef462d31ea30420e1b6

ARM: tegra: stop T124 LP0 breaking T20 LP0
The Tegra124 LP0 code introduces a header in a common location that
conflicts with a header that's specific to Tegra20 LP0, and relies
on that header being included in a file that's common to both. Fix
this by making parts of the latter common header conditional to
avoid always needing the conflicting header to be included. Ick.

Change-Id: I06e3c3461577d5262021c81dc3c17b16e0a0963e
Fixes: fba67d87d1c6 "ARM: tegra: add warmboot support for Tegra124"
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1754922
GVS: Gerrit_Virtual_Submit
Reviewed-by: Tom Warren <twarren@nvidia.com>

arch/arm/include/asm/arch-tegra/warmboot.h
@@ -7,7 +7,9 @@
 #ifndef _WARM_BOOT_H_
 #define _WARM_BOOT_H_
 
+#ifdef CONFIG_TEGRA124
 #include <asm/arch-tegra/sdram_param.h>
+#endif
 #include <mon_section_default.h>
 
 #define STRAP_OPT_A_RAM_CODE_SHIFT	4
@@ -224,6 +226,7 @@ void MON_SYM(wb_end)(void);	/* End of WB assembly code */
 
 /* Common routines for T114 and T124 SOCs */
 
+#ifdef CONFIG_TEGRA124
 /*
  * t1x4_wb_save_sdram_params():
  * save sdram parameters to scratch registers so sdram parameters can be
@@ -236,4 +239,5 @@ int MON_SYM(t1x4_wb_save_sdram_params)(struct sdram_params *sdram);
  * prepare WB code, which will be executed by AVP when system resumes from LP0
  */
 int MON_SYM(t1x4_wb_prepare_code)(u32 tegra_id, u32 seg_address, u32 seg_length);
+#endif
 #endif