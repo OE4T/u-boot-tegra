48a6e29ebe0447b5abac791f6c92f5c4c4c5ecdc
Tom Warren <twarren@nvidia.com>
2018-09-17 13:28:02 -0700

    ARM: tegra: Add p3450-Porg (Jetson-Nano) board support
    Porg is a p3448 CPU board married to a P3449 baseboard. The
    combination contains a T210 SoC, 2G LPDDR4, Max77620 PMIC, SPI,
    3x I2C, 2x USB3.0 and 1x USB3.1 HUB.
    
    Right now, this is a basic clone of p2371-2180 (TX1) that boots
    to the U-Boot command line on TX1/E2220. It will need more work
    (pinmux, real DTB, PMIC changes, etc.) to be able to boot a
    kernel/dtb from SD-card.
    
    Bug 200448064
    
    Change-Id: Ic60c921e709556c3cb1c7abf9990a7f7da2f7090
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1955696
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ include/configs/p3450-porg.h
@@ -0,0 +1,50 @@
+/*
+ * (C) Copyright 2018
+ * NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _P3450_PORG_H
+#define _P3450_PORG_H
+
+#include <linux/sizes.h>
+
+#include "tegra210-common.h"
+
+/* High-level configuration options */
+#define CONFIG_TEGRA_BOARD_STRING	"NVIDIA P3450-PORG"
+
+/* Board-specific serial config */
+#define CONFIG_TEGRA_ENABLE_UARTA
+
+/* I2C */
+#define CONFIG_SYS_I2C_TEGRA
+#define CONFIG_SYS_VI_I2C_TEGRA
+
+/* SD/MMC */
+#define CONFIG_MMC
+#define CONFIG_GENERIC_MMC
+#define CONFIG_TEGRA_MMC
+
+/* Environment s/b at end of SPI, fix it later */
+#define CONFIG_ENV_IS_NOWHERE
+
+/* SPI */
+#define CONFIG_SF_DEFAULT_MODE		SPI_MODE_0
+#define CONFIG_SF_DEFAULT_SPEED		24000000
+#define CONFIG_SPI_FLASH_SIZE		(4 << 20)
+
+/* PCI host support */
+#define CONFIG_PCI
+#define CONFIG_PCI_PNP
+#define CONFIG_CMD_PCI
+
+/* General networking support */
+
+#include "tegra-common-post.h"
+
+/* Crystal is 38.4MHz. clk_m runs at half that rate */
+#define COUNTER_FREQUENCY	19200000
+
+#endif /* _P3450_PORG_H */
