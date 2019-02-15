c8ca0eeefe047d09547e6b2481667cbd000f4fc5
Tom Warren <twarren@nvidia.com>
2019-02-15 09:23:10 -0800

    tegra: fdt: Restore fdt copy node/property code
    These routines were removed in commit aeff514b, and are now being
    restored to help with the goal of having U-Boot merge the DTB info
    found in the CBoot DTB passed in RAM with a kernel DTB found on the
    rootfs (SD-card on Porg, for instance).
    
    Currently the fdt_copy_env_proplist and fdt_copy_env_nodelist calls
    are not present in dt-setup.c. They will be added in a separate patch,
    along with env strings (in tegra210-common.h or a board-specific header
    like p3450-porg.h) that will indicate the properties or nodes that need
    to be copied into the disk-based kernel DTB before handing it off to
    the kernel.
    
    Bug 2481530
    
    Change-Id: I52836faf4f398128bfb848d4e98e527a1f0bee65
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2020445
    Reviewed-by: Stephen Warren <swarren@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- arch/arm/mach-tegra/dt-edit.h
+++ arch/arm/mach-tegra/dt-edit.h
@@ -1,8 +1,12 @@
 /*
- *  Copyright (C) 2010-2017 NVIDIA CORPORATION.
+ *  Copyright (C) 2010-2019 NVIDIA CORPORATION.
  *
  * SPDX-License-Identifier: GPL-2.0
  */
 
+void *fdt_copy_get_blob_src_default(void);
+
+int fdt_copy_env_proplist(void *blob_dst);
+int fdt_copy_env_nodelist(void *blob_dst);
 int fdt_del_env_nodelist(void *blob_dst);
 int fdt_del_env_proplist(void *blob_dst);
