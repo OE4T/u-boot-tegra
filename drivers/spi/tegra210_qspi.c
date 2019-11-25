86aedefabae307fff821632f1d6db53a64fa7354
Tom Warren <twarren@nvidia.com>
2019-11-25 14:43:50 -0700

    t210: qspi: Fix QSPI clock and tap delays
    When claim_bus was setting the clock, it reset the QSPI controller,
    which wipes out any tap delays set by previous bootloaders (nvtboot,
    CBoot for example on Nano). Instead of doing that in claim_bus, which
    gets called a lot, moved clock setting to probe(), and set tap delays
    there, too. Also updated clock to 80MHz to match CBoot. Now QSPI env
    save works reliably again.
    
    Bug 200568277
    
    Change-Id: I39a2d96ce7cc8c2842ea549a7025d25a6ea29abd
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2247295
    Reviewed-by: Stephen Warren <swarren@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/spi/tegra210_qspi.c
+++ drivers/spi/tegra210_qspi.c
@@ -41,10 +41,10 @@ DECLARE_GLOBAL_DATA_PTR;
 #define QSPI_CMD1_BITLEN_SHIFT		0
 
 /* COMMAND2 */
-#define QSPI_CMD2_TX_CLK_TAP_DELAY	BIT(6)
-#define QSPI_CMD2_TX_CLK_TAP_DELAY_MASK	GENMASK(11,6)
-#define QSPI_CMD2_RX_CLK_TAP_DELAY	BIT(0)
-#define QSPI_CMD2_RX_CLK_TAP_DELAY_MASK	GENMASK(5,0)
+#define QSPI_CMD2_TX_CLK_TAP_DELAY_SHIFT	10
+#define QSPI_CMD2_TX_CLK_TAP_DELAY_MASK	GENMASK(14,10)
+#define QSPI_CMD2_RX_CLK_TAP_DELAY_SHIFT	0
+#define QSPI_CMD2_RX_CLK_TAP_DELAY_MASK	GENMASK(7,0)
 
 /* TRANSFER STATUS */
 #define QSPI_XFER_STS_RDY		BIT(30)
@@ -126,10 +126,21 @@ static int tegra210_qspi_probe(struct udevice *bus)
 	struct tegra210_qspi_priv *priv = dev_get_priv(bus);
 
 	priv->regs = (struct qspi_regs *)plat->base;
+	struct qspi_regs *regs = priv->regs;
 
 	priv->last_transaction_us = timer_get_us();
 	priv->freq = plat->frequency;
 	priv->periph_id = plat->periph_id;
+
+	debug("%s: Freq = %u, id = %d\n", __func__, priv->freq, priv->periph_id);
+	/* Change SPI clock to correct frequency, PLLP_OUT0 source */
+	clock_start_periph_pll(priv->periph_id, CLOCK_ID_PERIPH, priv->freq);
+
+	/* Set tap delays here, clock change above resets QSPI controller */
+	u32 reg = (0x09 << QSPI_CMD2_TX_CLK_TAP_DELAY_SHIFT) |
+		   (0x0C << QSPI_CMD2_RX_CLK_TAP_DELAY_SHIFT);
+	writel(reg, &regs->command2);
+	debug("%s: COMMAND2 = %08x\n", __func__, readl(&regs->command2));
 
 	return 0;
 }
@@ -139,9 +150,6 @@ static int tegra210_qspi_claim_bus(struct udevice *dev)
 	struct udevice *bus = dev->parent;
 	struct tegra210_qspi_priv *priv = dev_get_priv(bus);
 	struct qspi_regs *regs = priv->regs;
-
-	/* Change SPI clock to correct frequency, PLLP_OUT0 source */
-	clock_start_periph_pll(priv->periph_id, CLOCK_ID_PERIPH, priv->freq);
 
 	debug("%s: FIFO STATUS = %08x\n", __func__, readl(&regs->fifo_status));
 
