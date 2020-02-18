7e55b54e61e9ec7ae05697b747fee35804e37cbe
Tom Warren <twarren@nvidia.com>
2020-02-18 14:51:07 -0700

    ARM: Tegra: Use calculated env var feature on all T186/T210 boards
    Large kernels, like those created w/GCOV, are failing to boot because
    they are overwriting the FDT address, corrupting the DTB. Stephen has
    created a fix to dynamically adjust the fdt/ramdisk/pxefile/kernel addr
    vars at boot time, which allows a large (I tested 66MB) kernel to load
    and boot.  Based on his commit 86fa169988959d4927533c532612b85a386483ee,
    but applied to the tegraXXX-common.h headers so it's used on all T186
    and T210 Jetson boards. Note that I've put the kernel 'size' param at
    0x03000000, or 48MB, to leave room for kernel growth. If a GCOV kernel
    is needed that exceeds that, change this to 0x04800000 (72MB), saveenv,
    and reboot.
    
    Bug 2832394
    Bug 200588621
    
    Change-Id: Ic8c2437ea34f9b92ee9e35208b9d4184eda263a5
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/u-boot/+/2291800
    Reviewed-by: Jonathan Hunter <jonathanh@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- include/configs/tegra186-common.h
+++ include/configs/tegra186-common.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright 2013-2019, NVIDIA CORPORATION.
+ * Copyright 2013-2020, NVIDIA CORPORATION.
  */
 
 #ifndef _TEGRA186_COMMON_H_
@@ -53,6 +53,24 @@
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"fdt_addr_r=0x82000000\0" \
 	"ramdisk_addr_r=0x82100000\0" \
+	"calculated_vars=kernel_addr_r fdt_addr_r scriptaddr pxefile_addr_r " \
+		"ramdisk_addr_r\0" \
+	"kernel_addr_r_align=00200000\0" \
+	"kernel_addr_r_offset=00080000\0" \
+	"kernel_addr_r_size=03000000\0" \
+	"kernel_addr_r_aliases=loadaddr\0" \
+	"fdt_addr_r_align=00200000\0" \
+	"fdt_addr_r_offset=00000000\0" \
+	"fdt_addr_r_size=00200000\0" \
+	"scriptaddr_align=00200000\0" \
+	"scriptaddr_offset=00000000\0" \
+	"scriptaddr_size=00200000\0" \
+	"pxefile_addr_r_align=00200000\0" \
+	"pxefile_addr_r_offset=00000000\0" \
+	"pxefile_addr_r_size=00200000\0" \
+	"ramdisk_addr_r_align=00200000\0" \
+	"ramdisk_addr_r_offset=00000000\0" \
+	"ramdisk_addr_r_size=02000000\0" \
 	"fdt_copy_node_paths=" \
 		"/chosen/plugin-manager:" \
 		"/chosen/reset:" \
