c372a3655bc3a50625ba6beabd55380fd79c16b2
Matt Madison <matt@madison.systems>
2020-04-23 08:06:09 -0700

    t210: add separate config for p3450-0002
    Local hack to have better support for the eMMC
    SKU until it arrives upstream.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- include/configs/p3450-0000.h
+++ include/configs/p3450-0000.h
@@ -10,25 +10,37 @@
 
 #include "tegra210-common.h"
 
-/* High-level configuration options */
-#define CONFIG_TEGRA_BOARD_STRING	"NVIDIA P3450-0000"
-
 /* Board-specific serial config */
 #define CONFIG_TEGRA_ENABLE_UARTA
+
+#ifdef CONFIG_P3450_EMMC
+#define CONFIG_TEGRA_BOARD_STRING	"NVIDIA P3450-0002"
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(PXE, pxe, na) \
+	func(DHCP, dhcp, na)
+#define CONFIG_SYS_MMC_ENV_DEV		0
+#define CONFIG_SYS_MMC_ENV_PART		2
+#define CONFIG_ENV_OFFSET		(-CONFIG_ENV_SIZE)
+#define DEFAULT_MMC_DEV			"0"
+#else
+/* High-level configuration options */
+#define CONFIG_TEGRA_BOARD_STRING	"NVIDIA P3450-0000"
 
 /* Only MMC/PXE/DHCP for now, add USB back in later when supported */
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 1) \
-	func(MMC, mmc, 0) \
 	func(PXE, pxe, na) \
 	func(DHCP, dhcp, na)
 
 /* Environment at end of QSPI, in the VER partition */
 #define CONFIG_SPI_FLASH_SIZE		(4 << 20)
+#define DEFAULT_MMC_DEV			"1"
+#endif
 
 #define BOARD_EXTRA_ENV_SETTINGS \
-	"preboot=if test -e mmc 1:1 /u-boot-preboot.scr; then " \
-		"load mmc 1:1 ${scriptaddr} /u-boot-preboot.scr; " \
+	"preboot=if test -e mmc " DEFAULT_MMC_DEV ":1 /u-boot-preboot.scr; then " \
+		"load mmc " DEFAULT_MMC_DEV ":1 ${scriptaddr} /u-boot-preboot.scr; " \
 		"source ${scriptaddr}; " \
 	"fi\0"
 
