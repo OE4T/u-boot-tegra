7a7e3f476ed048c1d693f2d117b93a071a4d7899
Tom Warren <twarren@nvidia.com>
2020-07-24 09:36:46 -0700

    ARM: tegra: Add NVIDIA Jetson Nano 2GB devkit support
    The Jetson Nano 2GB devkit is a Tegra X1-based development board. It is
    based on Jetson Nano but it is cost-reduced, with some baseboard
    components removed like the M.2 key, fan header, USB hub, DP, etc. It
    features 2GB of LPDDR4, a SPI NOR flash for early boot firmware and an
    SD card slot used for storage like Nano SKU0.
    
    HDMI is available for display, 3 USB ports (2 USB 2.0 and 1 USB 3.0) can
    be used to attach a variety of peripherals and a PCI Ethernet controller
    provides onboard network connectivity. Env save is at the end of QSPI
    (4MB-8K), same as Nano. Currently this build is just a clone of Nano
    U-Boot, with name changes to P3541. More customization will be done
    as needed.
    
    Bug 3072481
    
    Change-Id: Id4001ea06bbb7bfe1120613a56ef35c4b5241ffa
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/u-boot/+/2385369
    (cherry picked from commit 8f708d4202d9458cb1a842966f570ac426824c2b)
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/u-boot/+/2397029
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    GVS: Gerrit_Virtual_Submit

---
+++ board/nvidia/p3541-0000/p3541-0000.c
@@ -0,0 +1,141 @@
+/*
+ * (C) Copyright 2020
+ * NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <common.h>
+#include <i2c.h>
+#include <libfdt.h>
+#include <pca953x.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/pinmux.h>
+#include "../p2571/max77620_init.h"
+
+#define ETH_ALEN 6
+
+void pin_mux_mmc(void)
+{
+	struct udevice *dev;
+	uchar val;
+	int ret;
+
+	/* Turn on MAX77620 LDO2 to 3.3V for SD card power */
+	debug("%s: Set LDO2 for VDDIO_SDMMC_AP power to 3.3V\n", __func__);
+	ret = i2c_get_chip_for_busnum(0, MAX77620_I2C_ADDR_7BIT, 1, &dev);
+	if (ret) {
+		printf("%s: Cannot find MAX77620 I2C chip\n", __func__);
+		return;
+	}
+	/* 0xF2 for 3.3v, enabled: bit7:6 = 11 = enable, bit5:0 = voltage */
+	val = 0xF2;
+	ret = dm_i2c_write(dev, MAX77620_CNFG1_L2_REG, &val, 1);
+	if (ret)
+		printf("i2c_write 0 0x3c 0x27 failed: %d\n", ret);
+
+	/* Disable LDO4 discharge */
+	ret = dm_i2c_read(dev, MAX77620_CNFG2_L4_REG, &val, 1);
+	if (ret) {
+		printf("i2c_read 0 0x3c 0x2c failed: %d\n", ret);
+	} else {
+		val &= ~BIT(1); /* ADE */
+		ret = dm_i2c_write(dev, MAX77620_CNFG2_L4_REG, &val, 1);
+		if (ret)
+			printf("i2c_write 0 0x3c 0x2c failed: %d\n", ret);
+	}
+
+	/* Set MBLPD */
+	ret = dm_i2c_read(dev, MAX77620_CNFGGLBL1_REG, &val, 1);
+	if (ret) {
+		printf("i2c_write 0 0x3c 0x00 failed: %d\n", ret);
+	} else {
+		val |= BIT(6); /* MBLPD */
+		ret = dm_i2c_write(dev, MAX77620_CNFGGLBL1_REG, &val, 1);
+		if (ret)
+			printf("i2c_write 0 0x3c 0x00 failed: %d\n", ret);
+	}
+}
+
+#ifdef CONFIG_PCI_TEGRA
+int tegra_pcie_board_init(void)
+{
+	struct udevice *dev;
+	uchar val;
+	int ret;
+
+	/* Turn on MAX77620 LDO1 to 1.05V for PEX power */
+	debug("%s: Set LDO1 for PEX power to 1.05V\n", __func__);
+	ret = i2c_get_chip_for_busnum(0, MAX77620_I2C_ADDR_7BIT, 1, &dev);
+	if (ret) {
+		printf("%s: Cannot find MAX77620 I2C chip\n", __func__);
+		return -1;
+	}
+	/* 0xCA for 1.05v, enabled: bit7:6 = 11 = enable, bit5:0 = voltage */
+	val = 0xCA;
+	ret = dm_i2c_write(dev, MAX77620_CNFG1_L1_REG, &val, 1);
+	if (ret)
+		printf("i2c_write 0 0x3c 0x25 failed: %d\n", ret);
+
+	return 0;
+}
+#endif /* PCI */
+
+/*
+ * Attempt to use /chosen/nvidia,ethernet-mac in the cboot DTB as U-Boot's
+ * ethaddr environment variable if possible.
+ */
+int set_ethaddr_from_cboot(const void *fdt)
+{
+	int node, len, err;
+	const uchar *mac;
+
+	/* Already a valid address in the environment? If so, keep it */
+	if (getenv("ethaddr"))
+		return 0;
+
+	node = fdt_path_offset(fdt, "/chosen");
+	if (node < 0) {
+		printf("Can't find /chosen node in CBoot DTB\n");
+		return node;
+	}
+	mac = fdt_getprop(fdt, node, "nvidia,ethernet-mac", &len);
+	if (!mac) {
+		printf("Can't find nvidia,ethernet-mac property in CBoot DTB\n");
+		return -ENOENT;
+	}
+
+	debug("%s: MAC address: %s\n", __func__, mac);
+
+	/* Set MAC address */
+	err = setenv("ethaddr", (void *)mac);
+	if (err) {
+		printf("Failed to set ethaddr from CBoot DTB: %d\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+int ft_board_setup(void *fdt, bd_t *bd)
+{
+	uchar mac[ETH_ALEN];
+	int node, err;
+
+	node = fdt_path_offset(fdt, "/pcie@1003000/pci@2,0/ethernet@0,0");
+	if (node < 0)
+		return 0;
+
+	debug("PCI ethernet device tree node found\n");
+
+	if (eth_getenv_enetaddr("ethaddr", mac)) {
+		err = fdt_setprop(fdt, node, "mac-address", mac, ETH_ALEN);
+		if (err < 0)
+			return 0;
+
+		debug("MAC address set: %02x:%02x:%02x:%02x:%02x:%02x\n",
+		      mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	}
+
+	return 0;
+}
---
+++ include/configs/p3541-0000.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _P3541_0000_H
+#define _P3541_0000_H
+
+#include <linux/sizes.h>
+
+#include "tegra210-common.h"
+
+/* High-level configuration options */
+#define CONFIG_TEGRA_BOARD_STRING	"NVIDIA P3541-0000"
+
+/* Board-specific serial config */
+#define CONFIG_TEGRA_ENABLE_UARTA
+
+/* I2C */
+#define CONFIG_SYS_I2C_TEGRA
+#define CONFIG_SYS_VI_I2C_TEGRA
+
+/* SD/MMC */
+#define CONFIG_MMC
+#define CONFIG_GENERIC_MMC
+#define CONFIG_TEGRA_MMC
+
+/* Only MMC1/PXE/DHCP for now, add USB back in later when supported */
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 1) \
+	func(MMC, mmc, 0) \
+	func(PXE, pxe, na) \
+	func(DHCP, dhcp, na)
+
+/* Environment at end of QSPI, in the VER partition */
+#define CONFIG_ENV_IS_IN_SPI_FLASH
+#define CONFIG_ENV_SPI_MAX_HZ		48000000
+#define CONFIG_ENV_SPI_MODE		SPI_MODE_0
+#define CONFIG_ENV_SECT_SIZE		SZ_4K
+/* 4MB flash, environment located as high as possible */
+#define CONFIG_ENV_OFFSET		(SZ_4M - OFFSET_OF_UBOOT_ENV)
+
+/* SPI */
+#define CONFIG_SF_DEFAULT_MODE		SPI_MODE_0
+#define CONFIG_SF_DEFAULT_SPEED		24000000
+#define CONFIG_SPI_FLASH_SIZE		(4 << 20)
+
+/* USB2.0 Host support */
+#define CONFIG_USB_EHCI
+#define CONFIG_USB_EHCI_TEGRA
+
+/* PCI host support */
+#define CONFIG_PCI
+#define CONFIG_PCI_PNP
+#define CONFIG_CMD_PCI
+#define CONFIG_PREBOOT
+
+#define BOARD_EXTRA_ENV_SETTINGS \
+	"preboot=if test -e mmc 1:1 /u-boot-preboot.scr; then " \
+		"load mmc 1:1 ${scriptaddr} /u-boot-preboot.scr; " \
+		"source ${scriptaddr}; " \
+	"fi\0"
+
+/* General networking support */
+#include "tegra-common-usb-gadget.h"
+#include "tegra-common-post.h"
+
+/* Crystal is 38.4MHz. clk_m runs at half that rate */
+#define COUNTER_FREQUENCY	19200000
+
+/* Turn SDMMC1 off in early init on Porg/Nano */
+#define CONFIG_DISABLE_SDMMC1_EARLY
+
+#endif /* _P3541_0000_H */
