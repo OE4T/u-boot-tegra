9aa6c9ddd633ba18fcc0a20f51815ce61a22df47
Tom Warren <twarren@nvidia.com>
2020-11-10 16:16:27 -0700

    t186: Add NVMe support to TX2 and Lanai
    This will create a 'bootcmd_nvme0' on TX2/Lanai, which should allow
    the new NVME driver in v2020.04 U-Boot to boot from a NVME SSD in the
    M.2 slot on Lanai's CVB, or the PCIe slot on TX2.
    
    Bug 2509505
    
    Change-Id: I0b2b608f295e8f43091c666cd68d40a918baa8fd
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/u-boot/+/2444189
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>

--- include/configs/p2771-0000.h
+++ include/configs/p2771-0000.h
@@ -15,6 +15,15 @@
 
 /* Environment in eMMC, at the end of 2nd "boot sector" */
 
+/* SD, eMMC, USB, NVME, PXE, DHCP */
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 1) \
+	func(MMC, mmc, 0) \
+	func(USB, usb, 0) \
+	func(NVME, nvme, 0) \
+	func(PXE, pxe, na) \
+	func(DHCP, dhcp, na)
+
 #include "tegra-common-post.h"
 
 #endif
