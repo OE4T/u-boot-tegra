2f316fad8edf8fb616fb58bbba678af3d6c982de
Tom Warren <twarren@nvidia.com>
2020-12-10 22:16:33 -0700

    t210: xhci: driver cleanup, mostly PADCTL rework
    Uses fuse info to do pad calibration, board info to set up SS ports,
    etc. No more padctl slams.
    
    I'm now getting USB3.0 speeds during kernel load on TX1, Nano and Nano2GB
    (USB3.0 port J9).
    
    Bug 2505786
    
    Change-Id: I4a06c0255979768d2aa01659c2b02b10a767c241
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/u-boot/+/2460310
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/usb/host/xhci-tegra.c
+++ drivers/usb/host/xhci-tegra.c
@@ -266,69 +266,65 @@ static void tegra_pllu_enable(void)
 	/* TODO: Enable PLLU here */
 }
 
-void tegra_xusb_slam_padctl_init(void)
+static void padctl_usb3_port_init(int board_id)
 {
-	/*
-	 * FIXME: SLAM PADCTL REGS HERE USING KERNEL VALUES FOR NANO/TX1
-	 * Essentially does this:
-	 *  assign USB2 pads to XUSB (PADCTL)
-	 *  assign port caps for USB2 ports owned by XUSB (PADCTL)
-	 *  program PADCTL regs to keep UPHY in IDDQ
-	 *  assign UPHY lanes to XUSB (PADCTL)
-	 *  take UPHY out of IDDQ (PADCTL)
-	 *  assign static UPHY PAD/PLL params lanes (PADCTL)
-	 *  assign static UPHY PAD params of ports owned by XUSB (PADCTL)
-	 *  assign static USB2 PAD params to ports owned by XUSB (PADCTL)
-	 *  disable powerdown of USB2 ports owned by XUSB (PADCTL)
-	 *  release the XUSB SS wake logic state latches (PADCTL)
-	 *  bring lanes of UPHY out of IDDQ (only used lanes) (PADCTL)
-	 *  release always-on PAD muxing logic state latching (PADCTL)
-	 * This is a WAR because the U-Boot XUSB padctl driver isn't
-	 *  setting things up correctly at this time, need to debug & fix.
-	 */
-
-	int i;
+	int i, port;
 	u32 reg, val;
-	u32 *xusbpadctl = (u32 *)NV_ADDRESS_MAP_APB_XUSB_PADCTL_BASE;
-	u32 padctl_regs[] = {
-		/* reg		data */
-		0x0004, 0x00040055,		// USB2_PAD_MUX
-		0x0008, 0x00000113,		// USB2_PORT_CAP
-		0x0014, 0x00001CE1,		// SS_PORT_MAP
-		0x0020, 0x00C00100,		// ELPG_PROGRAM_0
-		0x0024, 0x000001F8,		// ELPG_PROGRAM_1
-		0x0028, 0x817FC0FE,		// USB3_PAD_MUX
-		0x0088, 0x00CD100E,		// USB2_OTG_PAD0_CTL_0
-		0x008C, 0x12100040,		// USB2_OTG_PAD0_CTL_1
-		0x00C8, 0x00CD1011,		// USB2_OTG_PAD1_CTL_0
-		0x00CC, 0x12100040,		// USB2_OTG_PAD1_CTL_1
-		0x0108, 0x00CD100E,		// USB2_OTG_PAD2_CTL_0
-		0x010C, 0x10100040,		// USB2_OTG_PAD2_CTL_1
-		0x0284, 0x260E003A,		// USB2_BIAS_PAD_CTL_0
-		0x0288, 0x0451E8E5,		// USB2_BIAS_PAD_CTL_1
-		0x0460, 0x01000080,		// UPHY_MISC_PAD_P0_CTL_1
-		0x04A0, 0x11000000,		// UPHY_MISC_PAD_P1_CTL_1
-		0x04E0, 0x11000000,		// UPHY_MISC_PAD_P2_CTL_1
-		0x0520, 0x11000000,		// UPHY_MISC_PAD_P3_CTL_1
-		0x0560, 0x11000000,		// UPHY_MISC_PAD_P4_CTL_1
-		0x0AA0, 0x0020001F,		// UPHY_USB3_PAD1_ECTL_1
-		0x0AA4, 0x000000FC,		// UPHY_USB3_PAD1_ECTL_2
-		0x0AA8, 0xC0077F1F,		// UPHY_USB3_PAD1_ECTL_3
-		0x0AB4, 0xFCF01368,		// UPHY_USB3_PAD1_ECTL_6
-		0x0C60, 0x0021505B,		// USB2_VBUS_ID
-	};
-
 	debug("%s: entry\n", __func__);
 
-	for (i = 0; i < sizeof(padctl_regs) / 4; i += 2) {
-		reg = padctl_regs[i];
-		val = padctl_regs[i + 1];
-		writel(val, xusbpadctl + (reg / 4));
-		debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
+	/* Set SS_PORT_MAP, SS PORT0 maps to USB2 PORT1 on all boards */
+	reg = XUSB_PADCTL_SS_PORT_MAP_0;
+	NV_XUSB_PADCTL_READ(SS_PORT_MAP, val);
+	debug("%s: reg %X: val read = %X\n", __func__, reg, val);
+	val &= ~SS_PORT_MAP(0, SS_PORT_MAP_PORT_DISABLED);
+	val |= SS_PORT_MAP(0, 1);
+	/* Set SS PORT1 maps to USB2 PORT2 on TX1 */
+	if (board_id == 2371) {
+		val &= ~SS_PORT_MAP(1, SS_PORT_MAP_PORT_DISABLED);
+		val |= SS_PORT_MAP(1, 2);
 	}
+	NV_XUSB_PADCTL_WRITE(SS_PORT_MAP, val);
+	debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
+
+	/* Set ELPG_PROGRAM_1 clamp bits next */
+	reg = XUSB_PADCTL_ELPG_PROGRAM_1_0;
+	NV_XUSB_PADCTL_READ(ELPG_PROGRAM_1, val);
+	debug("%s: reg %X: val read = %X\n", __func__, reg, val);
+
+	/* Unclamp SSP0 & SSP3 */
+	port = 0;
+	val &= ~SSPX_ELPG_CLAMP_EN(port);
+	val &= ~SSPX_ELPG_CLAMP_EN_EARLY(port);
+	val &= ~SSPX_ELPG_VCORE_DOWN(port);
+	port = 3;
+	val &= ~SSPX_ELPG_CLAMP_EN(port);
+	val &= ~SSPX_ELPG_CLAMP_EN_EARLY(port);
+	val &= ~SSPX_ELPG_VCORE_DOWN(port);
+	/* Unclamp PORT1 on TX1 */
+	if (board_id == 2371) {
+		port = 1;
+		val &= ~SSPX_ELPG_CLAMP_EN(port);
+		val &= ~SSPX_ELPG_CLAMP_EN_EARLY(port);
+		val &= ~SSPX_ELPG_VCORE_DOWN(port);
+	}
+	val &= ~AUX_MUX_LP0_CLAMP_EN;
+	val &= ~AUX_MUX_LP0_CLAMP_EN_EARLY;
+	val &= ~AUX_MUX_LP0_VCORE_DOWN;
+	NV_XUSB_PADCTL_WRITE(ELPG_PROGRAM_1, val);
+	debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
+
+	/* Set USB3_PAD_MUX, XUSB_PADCTL reset clears PCIE IDDQ bits 8-0 */
+	reg = XUSB_PADCTL_USB3_PAD_MUX_0;
+	NV_XUSB_PADCTL_READ(USB3_PAD_MUX, val);
+	debug("%s: reg %X: val read = %X\n", __func__, reg, val);
+	for (i = 0; i < 7; i++)
+		val |= FORCE_PCIE_PAD_IDDQ_DISABLE(i);
+	val |= FORCE_SATA_PAD_IDDQ_DISABLE(0);
+	NV_XUSB_PADCTL_WRITE(USB3_PAD_MUX, val);
+	debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
 }
 
-static void pad_trk_init(void)
+static void pad_trk_init(u32 hs_squelch)
 {
 	u32 val;
 
@@ -343,13 +339,19 @@ static void pad_trk_init(void)
 	NV_XUSB_PADCTL_WRITE(USB2_PAD_MUX, val);
 
 	NV_XUSB_PADCTL_READ(USB2_BIAS_PAD_CTL_1, val);
-	val &= ~(0x7F << TRK_START_TIMER);		/* START TIMER = 0 */
+	val &= ~(0x7F << TRK_START_TIMER);		/* START TIMER = 30 */
+	val |= (0x1E << TRK_START_TIMER);		/* DONE TIMER = 10 */
 	val &= ~(0x7F << TRK_DONE_RESET_TIMER);
 	val |= (0x0A << TRK_DONE_RESET_TIMER);		/* DONE TIMER = 10 */
 	NV_XUSB_PADCTL_WRITE(USB2_BIAS_PAD_CTL_1, val);
 
 	NV_XUSB_PADCTL_READ(USB2_BIAS_PAD_CTL_0, val);
 	val &= ~(1 << PD);				/* PD = 0 */
+	val &= ~BIAS_PAD_PD;
+	val &= ~HS_DISCON_LEVEL(~0);
+	val |= HS_DISCON_LEVEL(0x7);
+	val &= ~HS_SQUELCH_LEVEL(~0);
+	val |= HS_SQUELCH_LEVEL(hs_squelch);
 	NV_XUSB_PADCTL_WRITE(USB2_BIAS_PAD_CTL_0, val);
 
 	udelay(1);
@@ -357,6 +359,139 @@ static void pad_trk_init(void)
 	NV_XUSB_PADCTL_READ(USB2_BIAS_PAD_CTL_1, val);
 	val &= ~(1 << PD_TRK);				/* Enable tracking */
 	NV_XUSB_PADCTL_WRITE(USB2_BIAS_PAD_CTL_1, val);
+}
+
+static void do_padctl_usb2_config(void)
+{
+	int i, index;
+	u32 reg, val;
+	u32 *xusbpadctl = (u32 *)NV_ADDRESS_MAP_APB_XUSB_PADCTL_BASE;
+
+	u32 hs_curr_level[4];
+	u32 hs_curr_level_offset = 0;
+	u32 hs_squelch, hs_term_range_adj, rpd_ctrl;
+	u32 fuse_calib, fuse_calib_ext;
+
+	debug("%s: entry\n", __func__);
+
+	/* Set USB2 BIAS pad and USB2 OTG Port0 pad to 'XUSB' */
+	reg = XUSB_PADCTL_USB2_PAD_MUX_0;
+	NV_XUSB_PADCTL_READ(USB2_PAD_MUX, val);
+	debug("%s: reg %X: val read = %X\n", __func__, reg, val);
+	val &= ~(XUSB_PADCTL_USB2_PAD_MUX_USB2_BIAS_PAD_MASK <<
+		  XUSB_PADCTL_USB2_PAD_MUX_USB2_BIAS_PAD_SHIFT);
+	val |= XUSB_PADCTL_USB2_PAD_MUX_USB2_BIAS_PAD_XUSB <<
+		XUSB_PADCTL_USB2_PAD_MUX_USB2_BIAS_PAD_SHIFT;
+
+	val &= ~(XUSB_PADCTL_USB2_PAD_MUX_USB2_OTG_PAD_PORT0_MASK <<
+		  XUSB_PADCTL_USB2_PAD_MUX_USB2_OTG_PAD_PORT0_SHIFT);
+	val |= XUSB_PADCTL_USB2_PAD_MUX_USB2_OTG_PAD_PORT0_XUSB <<
+		XUSB_PADCTL_USB2_PAD_MUX_USB2_OTG_PAD_PORT0_SHIFT;
+	NV_XUSB_PADCTL_WRITE(USB2_PAD_MUX, val);
+	debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
+
+	/* Clear all SS/USB2/HSIC wakeup event bits */
+	val = 0x41E00780;
+	reg = XUSB_PADCTL_ELPG_PROGRAM_0_0;
+	NV_XUSB_PADCTL_WRITE(ELPG_PROGRAM_0, val);
+	debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
+
+	/* Set up USB2 pads */
+	reg = XUSB_PADCTL_USB2_PORT_CAP_0;
+	NV_XUSB_PADCTL_READ(USB2_PORT_CAP, val);
+	debug("%s: reg %X: val read = %X\n", __func__, reg, val);
+
+	/* PORT0 = OTG */
+	index = 0;
+	val &= ~XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_MASK(index);
+	val |= XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_OTG(index);
+	/* PORT1 = HOST */
+	index++;
+	val &= ~XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_MASK(index);
+	val |= XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_HOST(index);
+	/* PORT2 = HOST */
+	index++;
+	val &= ~XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_MASK(index);
+	val |= XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_HOST(index);
+	/* PORT3 = DISABLED */
+	index++;
+	val &= ~XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_MASK(index);
+	val |= XUSB_PADCTL_USB2_PORT_CAP_PORTX_CAP_DISABLED(index);
+	NV_XUSB_PADCTL_WRITE(USB2_PORT_CAP, val);
+	debug("%s: Wrote 0x%08X to 0x7009F%03X\n", __func__, val, reg);
+
+	/* Read USB calib fuse & ext values */
+	fuse_calib = readl(FUSE_USB_CALIB_0);
+	fuse_calib_ext = readl(FUSE_USB_CALIB_EXT_0);
+	debug("%s: FUSE_CALIB regs = 0x%08X/0x%08X\n", __func__, fuse_calib, fuse_calib_ext);
+
+	for (i = 0; i < ARRAY_SIZE(hs_curr_level); i++) {
+		hs_curr_level[i] = (fuse_calib >> HS_CURR_LEVEL_PADX_SHIFT(i)) &
+			HS_CURR_LEVEL_PAD_MASK;
+	}
+
+	hs_squelch = (fuse_calib >> HS_SQUELCH_SHIFT) & HS_SQUELCH_MASK;
+	hs_term_range_adj =
+		(fuse_calib >> HS_TERM_RANGE_ADJ_SHIFT) & HS_TERM_RANGE_ADJ_MASK;
+	rpd_ctrl = (fuse_calib_ext >> RPD_CTRL_SHIFT) & RPD_CTRL_MASK;
+
+	debug("%s: HS_SQUELCH = 0x%X, HS_TERM_RANGE_ADJ = 0x%X\n", __func__, hs_squelch, hs_term_range_adj);
+	debug(" RPD_CTRL = 0x%X\n", rpd_ctrl);
+
+	for (i = 0; i < ARRAY_SIZE(hs_curr_level); i++)
+		debug("HS_CURR_LEVEL[%d] = 0x%02X\n", i, hs_curr_level[i]);
+
+	for (index = 0; index < 3; index++) {
+		reg = XUSB_PADCTL_USB2_OTG_PADX_CTL_0(index);
+		val = readl(xusbpadctl + (reg / 4));
+		val &= ~USB2_OTG_PD;
+		val &= ~USB2_OTG_PD_ZI;
+		val &= ~HS_CURR_LEVEL(~0);
+		val |= HS_CURR_LEVEL(hs_curr_level[index] + hs_curr_level_offset);
+		writel(val, xusbpadctl + (reg / 4));
+		debug("%s: %d: Wrote 0x%08X to 0x7009F%03X\n", __func__, index, val, reg);
+
+		reg = XUSB_PADCTL_USB2_OTG_PADX_CTL_1(index);
+		val = readl(xusbpadctl + (reg / 4));
+		val &= ~TERM_RANGE_ADJ(~0);
+		val &= ~RPD_CTRL(~0);
+		val |= TERM_RANGE_ADJ(hs_term_range_adj);
+		val |= RPD_CTRL(rpd_ctrl);
+		val &= ~USB2_OTG_PD_DR;
+		writel(val, xusbpadctl + (reg / 4));
+		debug("%s: %d: Wrote 0x%08X to 0x7009F%03X\n", __func__, index, val, reg);
+	}
+
+	/* Setup pad tracking */
+	pad_trk_init(hs_squelch);
+}
+
+/* Simple hacky way to get board ID (3450, 3541, etc). */
+static int get_board_id(void)
+{
+	/* for ex, CONFIG_TEGRA_BOARD_STRING = "NVIDIA P3450-0000" */
+	const char *board = CONFIG_TEGRA_BOARD_STRING;
+	int board_num;
+
+	board_num = (int) simple_strtol(board+8, NULL, 10);
+	debug("%s: board = %d\n", __func__, board_num);
+
+	return board_num;
+}
+
+void do_tegra_xusb_padctl_init(void)
+{
+	int board_id;
+	debug("%s: entry\n", __func__);
+
+	board_id = get_board_id();
+	debug("%s: Board ID = %d\n", __func__, board_id);
+
+	/* Set up USB2 pads (MUX, CTLx, CAP, etc.) */
+	do_padctl_usb2_config();
+
+	/* Set up SS port map */
+	padctl_usb3_port_init(board_id);
 }
 
 void xhci_clk_rst_init(struct tegra_xhci *tegra)
@@ -456,11 +591,8 @@ static int tegra_xhci_core_init(struct tegra_xhci *tegra, u32 fw_addr)
 	mdelay(1);
 	reset_set_enable(PERIPH_ID_XUSB_PADCTL, 0);
 
-	/* Slam PADCTL registers w/default values per board (TX1/Nano) */
-	tegra_xusb_slam_padctl_init();
-
-	/* Setup pad tracking */
-	pad_trk_init();
+	/* Setup PADCTL registers w/default values per board (TX1/Nano) */
+	do_tegra_xusb_padctl_init();
 
 	/* Check PLLU, set up if not enabled & locked */
 	val = readl(&clkrst->crc_pll[CLOCK_ID_USB].pll_base);
@@ -531,7 +663,7 @@ static void tegra_xhci_core_exit(struct tegra_xhci *tegra)
 	reset_set_enable(PERIPH_ID_USBD, 1);
 	reset_set_enable(PERIPH_ID_USB2, 1);
 
-        /* XUSB_PADCTL_USB2_BIAS_PAD_CTL_0_0, power down the bias pad */
+	/* XUSB_PADCTL_USB2_BIAS_PAD_CTL_0_0, power down the bias pad */
 	NV_XUSB_PADCTL_READ(USB2_BIAS_PAD_CTL_0, val);
 	val |= (1 << PD);
 	NV_XUSB_PADCTL_WRITE(USB2_BIAS_PAD_CTL_0, val);
